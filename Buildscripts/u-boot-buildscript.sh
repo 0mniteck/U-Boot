7c44f70c92d98b2bb0f6fb8f7ab0555402a14121
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-07-05 20:42:09 -0700

    Update u-boot-buildscript.sh

--- Buildscripts/u-boot-buildscript.sh
+++ Buildscripts/u-boot-buildscript.sh
@@ -23,6 +23,9 @@ for dev in $BUILD_LIST
       cp /Includes/logo.bmp tools/logos/denx.bmp && cp /Includes/logo.bmp drivers/video/u_boot_logo.bmp && echo "Deployed Logo"
       if [ "$(echo $dev | cut -d':' -f2)" = "rockpro64-rk3399_defconfig" ]; then
         ../.././Configs/tpm-config.sh
+        sed -i '77idtb-$(CONFIG_ROCKCHIP_RK3399) += \\' arch/arm/dts/Makefile
+        sed -i '78irk3399-spi1-cs-gpio-slb9670.dtso' arch/arm/dts/Makefile
+        sed -i '79i\ ' arch/arm/dts/Makefile
         cp /Includes/rk3399-spi1-cs-gpio-slb9670.dtso dts/upstream/src/arm64/rockchip/rk3399-spi1-cs-gpio-slb9670.dtso && echo "Installed TPM Overlay"
       fi
       if [ "$(echo $dev | cut -d':' -f2)" = "pinebook-pro-rk3399_defconfig" ]; then
