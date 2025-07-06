e83bace5a0d37ef7a56266f2c5387d6b3b55704d
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-07-05 19:49:51 -0700

    Update sb-config.sh

--- Configs/sb-config.sh
+++ Configs/sb-config.sh
@@ -3,3 +3,4 @@
 echo "CONFIG_EFI_VARIABLE_NO_STORE=y" >> defconfig
 echo "CONFIG_EFI_VARIABLES_PRESEED=y" >> defconfig
 echo 'CONFIG_EFI_VAR_SEED_FILE="efi.var"' >> defconfig
+echo "CONFIG_ENV_IS_NOWHERE=y" >> defconfig
