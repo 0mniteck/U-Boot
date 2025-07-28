7f95b293eff34b14398109b247ce416fd7dbe938
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-07-28 03:22:34 -0700

    Update git.sh

--- git.sh
+++ git.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+GPG_TTY=$(tty)
+export GPG_TTY
+eval `ssh-agent -s`
+ssh-add $HOME/.ssh/id_ecdsa_s*[!.pub]
 git status && git add -A && git status
-git commit -a -S -m "$1" && sleep 5 && git push --set-upstream origin Docker
+git commit -a -S -m "$1" && sleep 5 && git push --set-upstream origin $(git rev-parse --abbrev-ref HEAD):Docker
 if [ "$2" != "" ]; then
   git tag -a "$2" -s -m "Tagged Release $2" && sleep 5 && git push origin "$2"
 fi
