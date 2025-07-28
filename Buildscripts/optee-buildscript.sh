274f9b7828c6a1db5f9bb290fa5c324311f6cf27
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-07-28 06:30:07 -0700

    Update optee-buildscript.sh

--- Buildscripts/optee-buildscript.sh
+++ Buildscripts/optee-buildscript.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 trap '[[ $pid ]] && kill $pid; exit' EXIT
-echo "Unzipping optee_os for $plat..."
-unzip -q $OPT_VER.zip > /dev/null
-echo "Entering /optee_os-$OPT_VER"
-pushd /optee_os-$OPT_VER
-  make -j $(nproc) PLATFORM=rockchip-rk3399 CFG_ARM64_core=y CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE32=arm-linux-gnueabihf- CROSS_COMPILE_core=aarch64-linux-gnu- CROSS_COMPILE_ta_arm32=arm-linux-gnueabihf- CROSS_COMPILE_ta_arm64=aarch64-linux-gnu-
-  ls -la /optee_os-$OPT_VER/out/arm-plat-rockchip/core/
-popd
+for plat in $ARCHS
+do
+  echo "Unzipping optee_os for $plat..."
+  unzip -q $OPT_VER.zip -d /$plat > /dev/null
+  echo "Entering /$plat/optee_os-$OPT_VER"
+  pushd /$plat/optee_os-$OPT_VER
+    make -j $(nproc) PLATFORM=rockchip-$plat CFG_ARM64_core=y CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE32=arm-linux-gnueabihf- CROSS_COMPILE_core=aarch64-linux-gnu- CROSS_COMPILE_ta_arm32=arm-linux-gnueabihf- CROSS_COMPILE_ta_arm64=aarch64-linux-gnu-
+    ls -la /$plat/optee_os-$OPT_VER/out/arm-plat-rockchip/core/
+  popd
+done
