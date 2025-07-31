564d4b4bbd730d1fc67b417ed262413dd0d52026
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-07-31 15:04:36 -0700

    Update xpl-config.sh

--- Configs/xpl-config.sh
+++ Configs/xpl-config.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
-echo "CONFIG_SPL_TINY_MEMSET=y" >> defconfig
 echo "CONFIG_ROCKCHIP_EXTERNAL_TPL=n" >> defconfig
-echo "CONFIG_SPL_LIBGENERIC_SUPPORT=n" >> defconfig
+echo "CONFIG_SPL_LIBGENERIC_SUPPORT=y" >> defconfig
 echo "CONFIG_SPL_BSS_MAX_SIZE=0x20000" >> defconfig
 echo "CONFIG_XPL_BUILD=y" >> defconfig
-##echo "CONFIG_TPL=y" >> defconfig
+echo "CONFIG_SPL_HAVE_INIT_STACK=y" >> defconfig
+echo "CONFIG_TPL=y" >> defconfig
 ##echo "CONFIG_TPL_BUILD=y" >> defconfig
 ##echo "CONFIG_SUPPORT_TPL=y" >> defconfig
 ##echo "CONFIG_TPL_MAX_SIZE=0x2800" >> defconfig
 #echo "CONFIG_SPL_MAX_SIZE=0x20000" >> defconfig
+#echo "CONFIG_SPL_TINY_MEMSET=y" >> defconfig
 ##echo "CONFIG_TPL_TINY_FRAMEWORK=y" >> defconfig
 ##echo "CONFIG_TPL_TINY_MEMSET=y" >> defconfig
 ##echo "CONFIG_TPL_NEEDS_SEPARATE_STACK=y" >> defconfig
