3107653dd9eb76c37c16a56c868c2491cae7afe4
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-09-07 02:27:23 -0700

    Update optee-buildscript.sh

--- Buildscripts/optee-buildscript.sh
+++ Buildscripts/optee-buildscript.sh
@@ -8,7 +8,7 @@ do
   mv /$plat/optee_os-$OPT_VER/ta/optee_ftpm-$OPT_VER /$plat/optee_os-$OPT_VER/ta/optee_ftpm
   pushd /$plat/optee_os-$OPT_VER/ta/optee_ftpm
     ls -la
-    make all
+    make -j $(nproc) CFG_MS_TPM_20_REF=/$plat/TPM all
   popd
   unzip -q TPM.zip -d /$plat/TPM > /dev/null
   mv /$plat/TPM/ms-tpm-20-ref-1.83r1/.* /$plat/TPM
