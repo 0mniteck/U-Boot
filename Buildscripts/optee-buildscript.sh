0bae4205f1d6642be25de0b3a562a5e931b67b4b
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-09-07 10:05:13 -0700

    Update optee-buildscript.sh

--- Buildscripts/optee-buildscript.sh
+++ Buildscripts/optee-buildscript.sh
@@ -9,13 +9,7 @@ do
   unzip -q TPM.zip -d /$plat/TPM > /dev/null
   mv /$plat/TPM/ms-tpm-20-ref-1.83r1/.* /$plat/TPM
   pushd /$plat/optee_os-$OPT_VER/ta/optee_ftpm
-    ls -la
-    export TA_DEV_KIT_DIR="/$plat/optee_os-$OPT_VER/ta"
-    export CFG_MS_TPM_20_REF="/$plat/TPM"
-    export CFG_TA_MEASURED_BOOT=y
-    export CFG_TA_EVENT_LOG_SIZE=65536
-    echo $CFG_MS_TPM_20_REF
-    make -j $(nproc) CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE32=arm-linux-gnueabihf- CROSS_COMPILE_ta_arm32=arm-linux-gnueabihf- CROSS_COMPILE_ta_arm64=aarch64-linux-gnu- all
+    make -j $(nproc) TA_DEV_KIT_DIR="/$plat/optee_os-$OPT_VER/ta" CFG_MS_TPM_20_REF="/$plat/TPM" CFG_TA_MEASURED_BOOT=y CFG_TA_EVENT_LOG_SIZE=65536 CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE32=arm-linux-gnueabihf- CROSS_COMPILE_ta_arm32=arm-linux-gnueabihf- CROSS_COMPILE_ta_arm64=aarch64-linux-gnu- all
   popd
   echo "Entering /$plat/optee_os-$OPT_VER"
   pushd /$plat/optee_os-$OPT_VER
