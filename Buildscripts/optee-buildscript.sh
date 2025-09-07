931bef985d345e59e2a7368cc1984a76df9375ea
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-09-07 02:18:37 -0700

    Update optee-buildscript.sh

--- Buildscripts/optee-buildscript.sh
+++ Buildscripts/optee-buildscript.sh
@@ -6,8 +6,12 @@ do
   unzip -q $OPT_VER.zip -d /$plat > /dev/null
   unzip -q ftpm_$OPT_VER.zip -d /$plat/optee_os-$OPT_VER/ta > /dev/null
   mv /$plat/optee_os-$OPT_VER/ta/optee_ftpm-$OPT_VER /$plat/optee_os-$OPT_VER/ta/optee_ftpm
-  unzip -q TPM.zip -d /TPM > /dev/null
-  mv /TPM/ms-tpm-20-ref-1.83r1/.* /TPM
+  pushd /$plat/optee_os-$OPT_VER/ta/optee_ftpm
+    ls -la
+    make all
+  popd
+  unzip -q TPM.zip -d /$plat/TPM > /dev/null
+  mv /$plat/TPM/ms-tpm-20-ref-1.83r1/.* /$plat/TPM
   echo "Entering /$plat/optee_os-$OPT_VER"
   pushd /$plat/optee_os-$OPT_VER
     make -j $(nproc) PLATFORM=rockchip-$plat CFG_ARM64_core=y CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE32=arm-linux-gnueabihf- CROSS_COMPILE_core=aarch64-linux-gnu- CROSS_COMPILE_ta_arm32=arm-linux-gnueabihf- CROSS_COMPILE_ta_arm64=aarch64-linux-gnu- \
