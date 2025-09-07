ba0782d91945b5f372fb359100ac0d476d3d1b30
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-09-06 22:14:37 -0700

    Update buildscript.sh

--- buildscript.sh
+++ buildscript.sh
@@ -7,6 +7,7 @@ export BASE_EXTRA="08-23-2025@sha256:362ba1531a982ccc0c474af60060a5854bfaaa480e9
 export OPT_VER="4.7.0"
 export OPT_SUM="a8753d9ca607c6f19e597cd0b084e5d053320ee38b7331e897dfda1bffc8212b3b75e6f74c0cd831cf6bc821d4fd0210d003389f480340fba157ff0cc7041e1e"
 export OPT_SUM2="a9a85451ee0cd140fd90a92ca03ade41adf9289128e0da12a80ab18da2771961a994c7817c313aeba497d6d96cfa3788b14da14cf88a3b4dabd25c94e6efec49"
+export TPM_SUM="411a186326447c4ef0dd23341f0ef6f0198efd6a8c930ad379338afef6093c43c848770a551b2f44c06e7464ac7726f3207a0799905a07ad13ce9874236ccc5d"
 export ATF_VER="lts-v2.12.5"
 export ATF_SUM="dfbd45eca6c437b82099d086dbce58b1615918b00e493b5e473950d43bb973599dba92d3d5ad148b841559520147e2745a07c68b959b901cf1c857e01134d302"
 export UB_VER="2025.07"
@@ -59,7 +60,7 @@ if [ "$TEST" = "yes" ]; then
 fi
 
 > vars.env
-for env in HUB^$HUB BASE^$BASE BASE_EXTRA^$BASE_EXTRA OPT_VER^$OPT_VER OPT_SUM^$OPT_SUM OPT_SUM2^$OPT_SUM2 ATF_VER^$ATF_VER ATF_SUM^$ATF_SUM UB_VER^$UB_VER UB_SUM^$UB_SUM
+for env in HUB^$HUB BASE^$BASE BASE_EXTRA^$BASE_EXTRA OPT_VER^$OPT_VER OPT_SUM^$OPT_SUM OPT_SUM2^$OPT_SUM2 TPM_SUM^$TPM_SUM ATF_VER^$ATF_VER ATF_SUM^$ATF_SUM UB_VER^$UB_VER UB_SUM^$UB_SUM
 do
   env1=$(echo $env | cut -d'^' -f1)
   env2=$(echo $env | cut -d'^' -f2)
@@ -94,4 +95,4 @@ git remote remove origin && git remote add origin git@UBoot:0mniteck/U-Boot.git
 ./clean.sh $CLEAN && sudo screen -c vars.env -L -Logfile builder.log bash -c './re-run.sh '$(($EPOCH))' '$CLEAN' '$TEST' '$AARCH
 echo "" && cat builder.log | grep -n "Checksum Matched! " && echo "" && cat Results/release.sha512sum && echo ""
 mv builder.log Results/builder.log && status="$(cat status.build)" && ./clean.sh cleanup && ls -la Builds/*
-read -p "$status: --> sign/commit/push" && ./git.sh "$status" "$TAG"export OPT_SUM=
+read -p "$status: --> sign/commit/push" && ./git.sh "$status" "$TAG"
