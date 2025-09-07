dd76c67ca1bd08d8f3d37649e51171eb36689cdb
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-09-06 23:20:13 -0700

    Update optee-buildscript.sh

--- Buildscripts/optee-buildscript.sh
+++ Buildscripts/optee-buildscript.sh
@@ -4,9 +4,14 @@ for plat in $ARCHS
 do
   echo "Unzipping optee_os for $plat..."
   unzip -q $OPT_VER.zip -d /$plat > /dev/null
+  unzip -q ftpm_$OPT_VER.zip -d /$plat/optee_os-$OPT_VER/ta > /dev/null
+  mv /$plat/optee_os-$OPT_VER/ta/optee_ftpm-$OPT_VER /$plat/optee_os-$OPT_VER/ta/optee_ftpm
+  unzip -q TPM.zip -d /TPM > /dev/null
+  mv /TPM/ms-tpm-20-ref-1.83r1/.* /TPM
   echo "Entering /$plat/optee_os-$OPT_VER"
   pushd /$plat/optee_os-$OPT_VER
-    make -j $(nproc) PLATFORM=rockchip-$plat CFG_ARM64_core=y CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE32=arm-linux-gnueabihf- CROSS_COMPILE_core=aarch64-linux-gnu- CROSS_COMPILE_ta_arm32=arm-linux-gnueabihf- CROSS_COMPILE_ta_arm64=aarch64-linux-gnu-
+    make -j $(nproc) PLATFORM=rockchip-$plat CFG_ARM64_core=y CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE32=arm-linux-gnueabihf- CROSS_COMPILE_core=aarch64-linux-gnu- CROSS_COMPILE_ta_arm32=arm-linux-gnueabihf- CROSS_COMPILE_ta_arm64=aarch64-linux-gnu- \
+    CFG_MS_TPM_20_REF="/TPM/" CFG_TA_MEASURED_BOOT=y
     ls -la /$plat/optee_os-$OPT_VER/out/arm-plat-rockchip/core/
   popd
 done
