4d7df52c41b9d15ebaba3116726ab13e21df9864
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-09-07 00:10:06 -0700

    Update common-config.sh

--- Configs/common-config.sh
+++ Configs/common-config.sh
@@ -8,6 +8,3 @@ echo "CONFIG_OPTEE=y" >> defconfig
 echo "CONFIG_OPTEE_TZDRAM_SIZE=0x02000000" >> defconfig
 echo "CONFIG_OPTEE_SERVICE_DISCOVERY=y" >> defconfig
 echo "CONFIG_BOOTM_OPTEE=y" >> defconfig
-echo "CONFIG_OPTEE_TA_SCP03=n" >> defconfig
-echo "CONFIG_OPTEE_TA_AVB=n" >> defconfig
-echo "CONFIG_CHIMP_OPTEE=n" >> defconfig
