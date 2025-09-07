3c93f57069c2d3c9049d0d592211217c669f6b52
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-09-06 23:25:49 -0700

    Update tpm-config.sh

--- Configs/tpm-config.sh
+++ Configs/tpm-config.sh
@@ -16,3 +16,5 @@ echo "CONFIG_SPL_TPM=y" >> defconfig
 # echo "CONFIG_TPM_RNG=y" >> defconfig
 echo "CONFIG_MEASURED_BOOT=y" >> defconfig
 echo "CONFIG_TPM_PCR_ALLOCATE=y" >> defconfig
+echo "CONFIG_EFI_TCG2_PROTOCOL=y" >> defconfig
+echo "CONFIG_EFI_TCG2_PROTOCOL_MEASURE_DTB=y" >> defconfig
