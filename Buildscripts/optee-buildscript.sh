bd644c78e59b33673cbcfb3f73ca4fb12c669011
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-09-08 01:15:48 -0700

    Update optee-buildscript.sh

--- Buildscripts/optee-buildscript.sh
+++ Buildscripts/optee-buildscript.sh
@@ -12,7 +12,7 @@ do
   # cp /$plat/optee_os-$OPT_VER/ta/mk/build-user-ta.mk /$plat/optee_os-$OPT_VER/mk/ta_dev_kit.mk
   # sed -i "s/PLATFORM_FLAVOR ?= rk322x/PLATFORM_FLAVOR ?= $plat/" /$plat/optee_os-$OPT_VER/core/arch/arm/plat-rockchip/conf.mk
   # cp /$plat/optee_os-$OPT_VER/core/arch/arm/plat-rockchip/conf.mk /$plat/optee_os-$OPT_VER/mk/conf.mk
-  pushd /$plat/optee_os-$OPT_VER/ta
+  pushd /$plat/optee_os-$OPT_VER/ta/optee_ftpm
     make -j $(nproc) TA_DEV_KIT_DIR=/$plat/optee_os-$OPT_VER/ta CFG_MS_TPM_20_REF=/$plat/TPM CFG_TA_MEASURED_BOOT=y CFG_TA_EVENT_LOG_SIZE=65536 CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE32=arm-linux-gnueabihf- CROSS_COMPILE_ta_arm32=arm-linux-gnueabihf- CROSS_COMPILE_ta_arm64=aarch64-linux-gnu- all
   popd
   echo "Entering /$plat/optee_os-$OPT_VER"
