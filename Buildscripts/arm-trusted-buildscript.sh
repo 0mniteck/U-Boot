5537abde695660618498cf1ce046f8c08c190f7a
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-09-14 02:35:34 -0700

    Update arm-trusted-buildscript.sh

--- Buildscripts/arm-trusted-buildscript.sh
+++ Buildscripts/arm-trusted-buildscript.sh
@@ -5,7 +5,7 @@ do
   unzip -q $ATF_VER.zip -d /$plat > /dev/null
   unzip -q mbedtls-$MTLS_VER.zip -d /$plat > /dev/null
   pushd /$plat/arm-trusted-firmware-$ATF_VER
-    make realclean && make BUILD_MESSAGE_TIMESTAMP="$(echo '"'$BUILD_MESSAGE_TIMESTAMP'"')" PLAT=$plat SPD=opteed MBEDTLS_DIR=/$plat/mbedtls-mbedtls-2.28.9 TRUSTED_BOARD_BOOT=1 GENERATE_COT=1 ARM_ROTPK_LOCATION=devel_rsa ROT_KEY=plat/arm/board/common/rotpk/arm_rotprivk_rsa.pem bl31
+    make realclean && make BUILD_MESSAGE_TIMESTAMP="$(echo '"'$BUILD_MESSAGE_TIMESTAMP'"')" PLAT=$plat SPD=opteed MBEDTLS_DIR=/$plat/mbedtls-mbedtls-$MTLS_VER TRUSTED_BOARD_BOOT=1 GENERATE_COT=1 ARM_ROTPK_LOCATION=devel_rsa ROT_KEY=plat/arm/board/common/rotpk/arm_rotprivk_rsa.pem bl31
     ls -la build/$plat/release/bl31/
   popd
 done
