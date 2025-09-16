13b52c2ecca467f3e4dff0ef5b190699e352fa74
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-09-16 02:53:39 -0700

    Update optee-buildscript.sh

--- Buildscripts/optee-buildscript.sh
+++ Buildscripts/optee-buildscript.sh
@@ -39,21 +39,21 @@ do
     sed -i "12i#include <stdio.h>" platform/NVMem.c
     sed -i "12d;83,97d;104d;105d" sub.mk
     sed -i "46icflags-y += -Wno-missing-include-dirs" sub.mk
-    sed -i "61icflags-platform/Clock.c-y += -Wno-nested-externs" sub.mk
-    sed -i "61icflags-platform/Clock.c-y += -Wno-missing-declarations" sub.mk
-    sed -i "61icflags-platform/Clock.c-y += -Wno-implicit-function-declaration" sub.mk
-    sed -i "61icflags-platform/Entropy.c-y += -Wno-nested-externs" sub.mk
-    sed -i "61icflags-platform/Entropy.c-y += -Wno-implicit-function-declaration" sub.mk
-    sed -i "61icflags-platform/RunCommand.c-y += -Wno-implicit-function-declaration" sub.mk
-    sed -i "61icflags-platform/RunCommand.c-y += -Wno-missing-declarations" sub.mk
-    sed -i "61icflags-platform/RunCommand.c-y += -Wno-builtin-declaration-mismatch" sub.mk
-    sed -i "61icflags-platform/NVMem.c-y += -Wno-int-conversion" sub.mk
-    sed -i "61icflags-platform/NVMem.c-y += -Wno-missing-declarations" sub.mk
-    sed -i "61icflags-platform/PlatformPcr.c-y += -Wno-old-style-definition" sub.mk
-    sed -i "61icflags-platform/PlatformPcr.c-y += -Wno-sign-compare" sub.mk
-    sed -i "61icflags-platform/VendorInfo.c-y += -Wno-missing-prototypes" sub.mk
-    sed -i "61icflags-platform/VendorInfo.c-y += -Wno-old-style-definition" sub.mk
-    sed -i "61icflags-platform/VendorInfo.c-y += -Wno-discarded-qualifiers" sub.mk
+    sed -i "60icflags-platform/Clock.c-y += -Wno-nested-externs" sub.mk
+    sed -i "60icflags-platform/Clock.c-y += -Wno-missing-declarations" sub.mk
+    sed -i "60icflags-platform/Clock.c-y += -Wno-implicit-function-declaration" sub.mk
+    sed -i "60icflags-platform/Entropy.c-y += -Wno-nested-externs" sub.mk
+    sed -i "60icflags-platform/Entropy.c-y += -Wno-implicit-function-declaration" sub.mk
+    sed -i "60icflags-platform/RunCommand.c-y += -Wno-implicit-function-declaration" sub.mk
+    sed -i "60icflags-platform/RunCommand.c-y += -Wno-missing-declarations" sub.mk
+    sed -i "60icflags-platform/RunCommand.c-y += -Wno-builtin-declaration-mismatch" sub.mk
+    sed -i "60icflags-platform/NVMem.c-y += -Wno-int-conversion" sub.mk
+    sed -i "60icflags-platform/NVMem.c-y += -Wno-missing-declarations" sub.mk
+    sed -i "60icflags-platform/PlatformPcr.c-y += -Wno-old-style-definition" sub.mk
+    sed -i "60icflags-platform/PlatformPcr.c-y += -Wno-sign-compare" sub.mk
+    sed -i "60icflags-platform/VendorInfo.c-y += -Wno-missing-prototypes" sub.mk
+    sed -i "60icflags-platform/VendorInfo.c-y += -Wno-old-style-definition" sub.mk
+    sed -i "60icflags-platform/VendorInfo.c-y += -Wno-discarded-qualifiers" sub.mk
     sed -i "60icflags-platform/VendorInfo.c-y += -Wno-missing-declarations" sub.mk
     sed -i "83i \\
 srcs-y += platform/Cancel.c\\
@@ -71,7 +71,6 @@ srcs-y += platform/PowerPlat.c\\
 srcs-y += platform/RunCommand.c\\
 srcs-y += platform/Unique.c\\
 srcs-y += platform/VendorInfo.c" sub.mk
-    cat sub.mk
     make -j $(nproc) MEASURED_BOOT=y TA_DEV_KIT_DIR=/$plat/optee_os-$OPT_VER/out/arm-plat-rockchip/export-ta_arm64 CFG_MS_TPM_20_REF=/$plat/TPM CFG_TA_MEASURED_BOOT=y CFG_USER_TA_TARGETS=ta_arm64 CFG_TA_EVENT_LOG_SIZE=65536 CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE32=arm-linux-gnueabihf- CROSS_COMPILE_ta_arm32=arm-linux-gnueabihf- CROSS_COMPILE_ta_arm64=aarch64-linux-gnu- O=out
   popd
   pushd /$plat/optee_os-$OPT_VER
