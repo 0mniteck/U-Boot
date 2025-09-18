27778cb780e3953862b82032cb9975b5fccd5e1d
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-09-18 04:35:21 -0700

    Update optee-buildscript.sh

--- Buildscripts/optee-buildscript.sh
+++ Buildscripts/optee-buildscript.sh
@@ -4,9 +4,11 @@ for plat in $ARCHS
 do
   unzip -q $OPT_VER.zip -d /$plat > /dev/null
   unzip -q ftpm_$OPT_VER.zip -d /$plat > /dev/null
-  unzip -q TPM.zip -d /$plat/TPM > /dev/null
-  mv /$plat/TPM/ms-tpm-20-ref-1.83r1/.* /$plat/TPM
-  mv /$plat/TPM/ms-tpm-20-ref-1.83r1/* /$plat/TPM
+  unzip -q TPM.zip -d /$plat > /dev/null
+  unzip -q SSL.zip -d /$plat > /dev/null
+  mv /$plat/ms-tpm-20-ref-1.83r1 /$plat/TPM
+  mkdir -p /$plat/openssl/
+  mv /$plat/openssl-openssl-$SSL_VER/crypto /$plat/openssl/openssl
   pushd /$plat/optee_os-$OPT_VER
     make -j $(nproc) PLATFORM=rockchip-$plat CFG_ARM64_core=y CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE32=arm-linux-gnueabihf- CROSS_COMPILE_core=aarch64-linux-gnu- CROSS_COMPILE_ta_arm32=arm-linux-gnueabihf- CROSS_COMPILE_ta_arm64=aarch64-linux-gnu- CFG_USER_TA_TARGETS=ta_arm64 CFG_EARLY_CONSOLE_BAUDRATE=115200 EARLY_TA_PATHS=/$plat/optee_ftpm-$OPT_VER/out/bc50d971-d4c9-42c4-82cb-343fb7f37896.stripped.elf ta_dev_kit
   popd
@@ -44,26 +46,29 @@ do
     sed -i "35iglobal-incdirs_ext-y += \$(CFG_MS_TPM_20_REF)/TPMCmd/tpm/include/private/prototypes" sub.mk
     sed -i "35iglobal-incdirs_ext-y += \$(CFG_MS_TPM_20_REF)/TPMCmd/tpm/include/public" sub.mk
     sed -i "35iglobal-incdirs_ext-y += \$(CFG_MS_TPM_20_REF)/TPMCmd/tpm/include/public/prototypes" sub.mk
+    sed -i "35iglobal-incdirs_ext-y += /$plat/openssl" sub.mk
     sed -i "35iglobal-incdirs_ext-y += /usr/include/aarch64-linux-gnu" sub.mk
     sed -i "35iglobal-incdirs_ext-y += /usr/include" sub.mk
-    sed -i "52icflags-y += -Wno-missing-include-dirs" sub.mk
-    sed -i "72icflags-platform/Clock.c-y += -Wno-nested-externs" sub.mk
-    sed -i "72icflags-platform/Clock.c-y += -Wno-missing-declarations" sub.mk
-    sed -i "72icflags-platform/Clock.c-y += -Wno-implicit-function-declaration" sub.mk
-    sed -i "72icflags-platform/Entropy.c-y += -Wno-nested-externs" sub.mk
-    sed -i "72icflags-platform/Entropy.c-y += -Wno-implicit-function-declaration" sub.mk
-    sed -i "72icflags-platform/RunCommand.c-y += -Wno-implicit-function-declaration" sub.mk
-    sed -i "72icflags-platform/RunCommand.c-y += -Wno-missing-declarations" sub.mk
-    sed -i "72icflags-platform/RunCommand.c-y += -Wno-builtin-declaration-mismatch" sub.mk
-    sed -i "72icflags-platform/NVMem.c-y += -Wno-int-conversion" sub.mk
-    sed -i "72icflags-platform/NVMem.c-y += -Wno-missing-declarations" sub.mk
-    sed -i "72icflags-platform/PlatformPcr.c-y += -Wno-old-style-definition" sub.mk
-    sed -i "72icflags-platform/PlatformPcr.c-y += -Wno-sign-compare" sub.mk
-    sed -i "72icflags-platform/VendorInfo.c-y += -Wno-missing-prototypes" sub.mk
-    sed -i "72icflags-platform/VendorInfo.c-y += -Wno-old-style-definition" sub.mk
-    sed -i "72icflags-platform/VendorInfo.c-y += -Wno-discarded-qualifiers" sub.mk
-    sed -i "72icflags-platform/VendorInfo.c-y += -Wno-missing-declarations" sub.mk
-    sed -i "111i \\
+    sed -i "53icflags-y += -Wno-missing-include-dirs" sub.mk
+    sed -i "53icflags-y += -Wno-strict-aliasing" sub.mk
+    sed -i "53icflags-y += -Wno-nested-externs" sub.mk
+    sed -i "75icflags-platform/Clock.c-y += -Wno-nested-externs" sub.mk
+    sed -i "75icflags-platform/Clock.c-y += -Wno-missing-declarations" sub.mk
+    sed -i "75icflags-platform/Clock.c-y += -Wno-implicit-function-declaration" sub.mk
+    sed -i "75icflags-platform/Entropy.c-y += -Wno-nested-externs" sub.mk
+    sed -i "75icflags-platform/Entropy.c-y += -Wno-implicit-function-declaration" sub.mk
+    sed -i "75icflags-platform/RunCommand.c-y += -Wno-implicit-function-declaration" sub.mk
+    sed -i "75icflags-platform/RunCommand.c-y += -Wno-missing-declarations" sub.mk
+    sed -i "75icflags-platform/RunCommand.c-y += -Wno-builtin-declaration-mismatch" sub.mk
+    sed -i "75icflags-platform/NVMem.c-y += -Wno-int-conversion" sub.mk
+    sed -i "75icflags-platform/NVMem.c-y += -Wno-missing-declarations" sub.mk
+    sed -i "75icflags-platform/PlatformPcr.c-y += -Wno-old-style-definition" sub.mk
+    sed -i "75icflags-platform/PlatformPcr.c-y += -Wno-sign-compare" sub.mk
+    sed -i "75icflags-platform/VendorInfo.c-y += -Wno-missing-prototypes" sub.mk
+    sed -i "75icflags-platform/VendorInfo.c-y += -Wno-old-style-definition" sub.mk
+    sed -i "75icflags-platform/VendorInfo.c-y += -Wno-discarded-qualifiers" sub.mk
+    sed -i "75icflags-platform/VendorInfo.c-y += -Wno-missing-declarations" sub.mk
+    sed -i "114i \\
 srcs-y += platform/Cancel.c\\
 srcs-y += platform/Clock.c\\
 srcs-y += platform/DebugHelpers.c\\
@@ -301,7 +306,7 @@ srcs_ext-y += support/TableMarshalData.c
 srcs_ext-y += support/TpmFail.c
 srcs_ext-y += support/TpmSizeChecks.c" >> sub.mk
     cat sub.mk
-    make -j $(nproc) MEASURED_BOOT=y TA_DEV_KIT_DIR=/$plat/optee_os-$OPT_VER/out/arm-plat-rockchip/export-ta_arm64 CFG_MS_TPM_20_REF=/$plat/TPM CFG_TA_MEASURED_BOOT=y CFG_USER_TA_TARGETS=ta_arm64 CFG_TA_EVENT_LOG_SIZE=65536 CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE32=arm-linux-gnueabihf- CROSS_COMPILE_ta_arm32=arm-linux-gnueabihf- CROSS_COMPILE_ta_arm64=aarch64-linux-gnu- O=out
+    make -j $(nproc) ALG_ECC=y ECC_NIST_P384=y ALG_RSA=y RSA_4096=y MEASURED_BOOT=y TA_DEV_KIT_DIR=/$plat/optee_os-$OPT_VER/out/arm-plat-rockchip/export-ta_arm64 CFG_MS_TPM_20_REF=/$plat/TPM CFG_TA_MEASURED_BOOT=y CFG_USER_TA_TARGETS=ta_arm64 CFG_TA_EVENT_LOG_SIZE=65536 CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE32=arm-linux-gnueabihf- CROSS_COMPILE_ta_arm32=arm-linux-gnueabihf- CROSS_COMPILE_ta_arm64=aarch64-linux-gnu- O=out
     read -p "Waiting fot user..."
   popd
   pushd /$plat/optee_os-$OPT_VER
