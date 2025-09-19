b9dbc4dcba914d932d4077b846fc3ecf3cfcdbf4
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-09-19 03:27:52 -0700

    Update optee-buildscript.sh

--- Buildscripts/optee-buildscript.sh
+++ Buildscripts/optee-buildscript.sh
@@ -36,8 +36,8 @@ do
     sed -i "s'ECC_CURVE_DATA'TPM_ECC_CURVE'" include/TEE/TpmToTEEMath.h
     sed -i "3d;12d;36d;83,97d;104,105d;110,309d" sub.mk
     sed -i "11iexport CC=gcc" sub.mk
-    sed -i "11icppflags-y += -include TpmAlgorithmDefines.h"  sub.mk
-    sed -i "18a -DBN_MATH_LIB=Ossl" sub.mk
+    sed -i "11icppflags-y += -include TpmAlgorithmDefines.h" sub.mk
+    sed -i "18 a'$'-DBN_MATH_LIB=Ossl'" sub.mk
     # sed -i "35iglobal-incdirs_ext-y += \$(CFG_MS_TPM_20_REF)/TPMCmd/tpm/cryptolibs/wolf/include" sub.mk
     sed -i "35iglobal-incdirs_ext-y += \$(CFG_MS_TPM_20_REF)/TPMCmd/tpm/cryptolibs/TpmBigNum/include" sub.mk
     sed -i "35iglobal-incdirs_ext-y += \$(CFG_MS_TPM_20_REF)/TPMCmd/tpm/cryptolibs/Ossl/include" sub.mk
