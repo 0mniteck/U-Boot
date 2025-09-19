3dd6bca49504940dd71f0d5711d3cda1f187774d
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-09-19 08:15:10 -0700

    Update optee-buildscript.sh

--- Buildscripts/optee-buildscript.sh
+++ Buildscripts/optee-buildscript.sh
@@ -36,7 +36,7 @@ do
     sed -i "s'XYZ 'OMNITECK '" platform/VendorInfo.c
     sed -i "s'xCG 'TCG '" platform/VendorInfo.c
     sed -i "s'ECC_CURVE_DATA'TPM_ECC_CURVE'" include/TEE/TpmToTEEMath.h
-    sed -i "5d" platform/include/Platform.h
+    sed -i "5d;7d" platform/include/Platform.h
     sed -i "3d;12d;36d;83,97d;104,105d;110,309d" sub.mk
     sed -i "11iexport CC=gcc" sub.mk
     sed -i "s'-DMATH_LIB=TEE'-DMATH_LIB=TpmBigNum'" sub.mk
