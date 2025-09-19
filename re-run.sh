95606b8125f4115621108524ccff71e00b566dfa
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-09-19 11:55:06 -0700

    Update re-run.sh

--- re-run.sh
+++ re-run.sh
@@ -1,6 +1,33 @@
 #!/bin/bash
 
+if [ "$2" = "no" ]; then
+  echo "CLEAN_BUILD: $2"
+fi
+if [ "$3" = "yes" ]; then
+  echo "DEV_BUILD: $3"
+  load() { # $1 = Location/Name
+    export LOAD="--platform linux/arm64 --target $1 --tag $1"
+    export NAME=$1
+    return
+    }
+else
+  load() { # $1 = Location/Name
+    export LOAD="--load --metadata-file Results/$1.meta.json --platform linux/arm64 --target $1 --tag $1"
+    export BUILDX_METADATA_PROVENANCE=max
+    export NAME=$1
+    return
+    }
+fi
+if [ "$4" = "yes" ]; then
+  echo "CROSS_COMPILE: $4"
+fi
+echo "SOURCE_DATE: $source_date"
+echo "SOURCE_DATE_EPOCH: $source_date_epoch"
+echo "BUILD_MESSAGE_TIMESTAMP: $build_message_timestamp"
+ARCHS=$(echo $ARCHS | tr ' ' '\n' | sort -u | tr '\n' ' ')
 echo "# Starting Build: $(date -u '+on %D at %R UTC')" >> Results/release.sha512sum && echo "" >> Results/release.sha512sum && echo "Starting Build: $(date -u '+on %D at %R UTC')"
+echo '' > Results/release.sha512sum && echo '' > Results/release.sha3sum
+
 sudo apt install -y snapd
 snap install syft --classic
 snap install grype --classic
@@ -37,52 +64,43 @@ stop() {
   docker stop $1 > /dev/null && echo "$1 stopped" && docker rm --volumes $1 > /dev/null && echo "$1 removed"
 }
 
-scan_using_grype() { # $1 = Name, $2 = Type:[Name]
-  pushd Results/
-    if [ -f "$HOME/.grype.yaml" ]; then GRCONF="-c $HOME/.grype.yaml"; fi
-    mkdir -p "$HOME/syft" && TMPDIR="$HOME/syft" syft scan $2 -o spdx-json=$1.spdx.json && rm -f -r "$HOME/syft"
-    script -q -c "grype $GRCONF sbom:$1.spdx.json -o json > $1.grype.json" $1.grype.tmp
-    grep "✔ Scanned for vulnerabilities" $1.grype.tmp | tail -n 1 > $1.grype.status.1
-    tr -d '\000-\037\177' < $1.grype.status.1 | sed '/^$/d' > $1.grype.status.1.tmp
-    line1=$(cat $1.grype.status.1.tmp)
-    left1=${line1%%" [K"*}
-    grep "├── by severity:" $1.grype.tmp | tail -n 1 > $1.grype.status.2
-    tr -d '\000-\037\177' < $1.grype.status.2 | sed '/^$/d' > $1.grype.status.2.tmp
-    line2=$(cat $1.grype.status.2.tmp)
-    left2=${line2%%" [K"*}
-    grep "└── by status:" $1.grype.tmp | tail -n 1 > $1.grype.status.3
-    tr -d '\000-\037\177' < $1.grype.status.3 | sed '/^$/d' > $1.grype.status.3.tmp
-    line3=$(cat $1.grype.status.3.tmp)
-    left3=${line3%%" [K"*}
-    echo $left1 > $1.grype.status
-    echo $left2 >> $1.grype.status
-    echo $left3 >> $1.grype.status
-    rm -f $1.grype.tmp
-    rm -f $1.grype.status.*
-    cat $1.grype.status
-  popd
+scan_using_grype() { # $1 = Name, $2 = Type:[Name], $3 = $3
+  if [ "$3" != "yes" ]; then
+    pushd Results/
+      if [ -f "$HOME/.grype.yaml" ]; then GRCONF="-c $HOME/.grype.yaml"; fi
+      mkdir -p "$HOME/syft" && TMPDIR="$HOME/syft" syft scan $2 -o spdx-json=$1.spdx.json && rm -f -r "$HOME/syft"
+      script -q -c "grype $GRCONF sbom:$1.spdx.json -o json > $1.grype.json" $1.grype.tmp
+      grep "✔ Scanned for vulnerabilities" $1.grype.tmp | tail -n 1 > $1.grype.status.1
+      tr -d '\000-\037\177' < $1.grype.status.1 | sed '/^$/d' > $1.grype.status.1.tmp
+      line1=$(cat $1.grype.status.1.tmp)
+      left1=${line1%%" [K"*}
+      grep "├── by severity:" $1.grype.tmp | tail -n 1 > $1.grype.status.2
+      tr -d '\000-\037\177' < $1.grype.status.2 | sed '/^$/d' > $1.grype.status.2.tmp
+      line2=$(cat $1.grype.status.2.tmp)
+      left2=${line2%%" [K"*}
+      grep "└── by status:" $1.grype.tmp | tail -n 1 > $1.grype.status.3
+      tr -d '\000-\037\177' < $1.grype.status.3 | sed '/^$/d' > $1.grype.status.3.tmp
+      line3=$(cat $1.grype.status.3.tmp)
+      left3=${line3%%" [K"*}
+      echo $left1 > $1.grype.status
+      echo $left2 >> $1.grype.status
+      echo $left3 >> $1.grype.status
+      rm -f $1.grype.tmp
+      rm -f $1.grype.status.*
+      cat $1.grype.status
+    popd
+  else
+    return
+  fi
 }
 
-if [ "$2" = "no" ]; then
-  echo "CLEAN_BUILD: $2"
-fi
-if [ "$3" = "yes" ]; then
-  echo "DEV_BUILD: $3"
-fi
-if [ "$4" = "yes" ]; then
-  echo "CROSS_COMPILE: $4"
-fi
-echo "SOURCE_DATE: $source_date"
-echo "SOURCE_DATE_EPOCH: $source_date_epoch"
-echo "BUILD_MESSAGE_TIMESTAMP: $build_message_timestamp"
-ARCHS=$(echo $ARCHS | tr ' ' '\n' | sort -u | tr '\n' ' ')
-echo '' > Results/release.sha512sum && echo '' > Results/release.sha3sum
 docker buildx create --name U-Boot-Builder --platform linux/arm64 --driver-opt "network=host" --bootstrap --use
 if [ "$4" = "yes" ]; then
   docker run --privileged --rm tonistiigi/binfmt --install all
 fi
 if [ "$2" = "yes" ]; then
-  docker buildx build --load --platform linux/arm64 --target optee --tag optee \
+  load optee
+  docker buildx build $LOAD \
     --build-arg SOURCE_DATE_EPOCH=$source_date_epoch \
     --build-arg OPT_VER=$OPT_VER \
     --build-arg OPT_SUM=$OPT_SUM \
@@ -94,35 +112,36 @@ if [ "$2" = "yes" ]; then
     --build-arg HUB=$HUB \
     --build-arg BASE=$BASE \
     --build-arg BASE_EXTRA=$BASE_EXTRA \
-    --build-arg ENTRYPOINT=optee \
+    --build-arg ENTRYPOINT=$NAME \
     -f Dockerfile .
 
-  scan_using_grype optee-os docker:optee
+  scan_using_grype $NAME docker:$NAME $3
 
   docker run -it --cpus=$(nproc) \
-    --name optee \
+    --name $NAME \
     --platform linux/arm64 \
     --user "$(id -u):$(id -g)" \
-    --entrypoint /optee-buildscript.sh \
+    --entrypoint /$NAME-buildscript.sh \
     -e SOURCE_DATE_EPOCH=$source_date_epoch \
     -e SSL_VER=$SSL_VER \
     -e OPT_VER=$OPT_VER \
     -e ARCHS="$ARCHS" \
-    optee
+    $NAME
 
   for arch in $ARCHS
   do
     for tpm in ":-tpm" "/NOTPM:"
     do
       tpm=$(echo $tpm | cut -d':' -f2)
-      docker cp optee:$(echo $tpm | cut -d':' -f1)/$arch/optee_os-$OPT_VER/out/arm-plat-rockchip/core/tee.bin Builds/$arch/tee$tpm.bin
+      docker cp $NAME:$(echo $tpm | cut -d':' -f1)/$arch/optee_os-$OPT_VER/out/arm-plat-rockchip/core/tee.bin Builds/$arch/tee$tpm.bin
       sha512sum Builds/$arch/tee$tpm.bin && sha512sum Builds/$arch/tee$tpm.bin >> Results/release.sha512sum
       openssl dgst -SHA3-256 Builds/$arch/tee$tpm.bin && openssl dgst -SHA3-256 Builds/$arch/tee$tpm.bin >> Results/release.sha3sum
     done
   done
-  stop optee
-  
-  docker buildx build --load --platform linux/arm64 --target arm-trusted --tag arm-trusted \
+  stop $NAME
+
+  load arm-trusted
+  docker buildx build $LOAD \
     --build-arg SOURCE_DATE_EPOCH=$source_date_epoch \
     --build-arg BUILD_MESSAGE_TIMESTAMP="$build_message_timestamp" \
     --build-arg ATF_VER=$ATF_VER \
@@ -132,69 +151,70 @@ if [ "$2" = "yes" ]; then
     --build-arg HUB=$HUB \
     --build-arg BASE=$BASE \
     --build-arg BASE_EXTRA=$BASE_EXTRA \
-    --build-arg ENTRYPOINT=arm-trusted \
+    --build-arg ENTRYPOINT=$NAME \
     -f Dockerfile .
 
-  scan_using_grype arm-trusted-firmware docker:arm-trusted
+  scan_using_grype $NAME docker:$NAME $3
 
   docker run -it --cpus=$(nproc) \
-    --name arm-trusted \
+    --name $NAME \
     --platform linux/arm64 \
     --user "$(id -u):$(id -g)" \
-    --entrypoint /arm-trusted-buildscript.sh \
+    --entrypoint /$NAME-buildscript.sh \
     -e SOURCE_DATE_EPOCH=$source_date_epoch \
     -e BUILD_MESSAGE_TIMESTAMP="$build_message_timestamp" \
     -e ATF_VER=$ATF_VER \
     -e ARCHS="$ARCHS" \
-    arm-trusted
+    $NAME
 
   for arch in $ARCHS
   do
-    docker cp arm-trusted:/$arch/arm-trusted-firmware-$ATF_VER/build/$arch/release/bl31/bl31.elf Builds/$arch/
+    docker cp $NAME:/$arch/arm-trusted-firmware-$ATF_VER/build/$arch/release/bl31/bl31.elf Builds/$arch/
     sha512sum Builds/$arch/bl31.elf && sha512sum Builds/$arch/bl31.elf >> Results/release.sha512sum
     openssl dgst -SHA3-256 Builds/$arch/bl31.elf && openssl dgst -SHA3-256 Builds/$arch/bl31.elf >> Results/release.sha3sum
   done
-  stop arm-trusted
+  stop $NAME
 fi
 
-docker buildx build --load --platform linux/arm64 --target u-boot --tag u-boot \
+load u-boot
+docker buildx build $LOAD \
   --build-arg SOURCE_DATE_EPOCH=$source_date_epoch \
   --build-arg UB_VER=$UB_VER \
   --build-arg UB_SUM=$UB_SUM \
   --build-arg HUB=$HUB \
   --build-arg BASE=$BASE \
   --build-arg BASE_EXTRA=$BASE_EXTRA \
-  --build-arg ENTRYPOINT=u-boot \
+  --build-arg ENTRYPOINT=$NAME \
   -f Dockerfile .
 
-scan_using_grype u-boot docker:u-boot
+scan_using_grype $NAME docker:$NAME $3
 
 docker run -it --cpus=$(nproc) \
-  --name u-boot \
+  --name $NAME \
   --platform linux/arm64 \
   --user "$(id -u):$(id -g)" \
-  --entrypoint /u-boot-buildscript.sh \
+  --entrypoint /$NAME-buildscript.sh \
   -e SOURCE_DATE_EPOCH=$source_date_epoch \
   -e SOURCE_DATE=$source_date \
   -e UB_VER=$UB_VER \
   -e BUILD_LIST="$BUILD_LIST" \
   -e DEV_BUILD=$3 \
-  u-boot
+  $NAME
 
 for dev in $LIST
 do
-  for loc in $dev $dev-SB $dev-MU-SB
+  for loc in $dev $dev-SB $dev-TPM-SB $dev-MU-SB
   do
-    docker cp u-boot:/$loc/ Builds
+    docker cp $NAME:/$loc/ Builds
     sha512sum Builds/$loc/u-boot-rockchip.bin && sha512sum Builds/$loc/u-boot-rockchip.bin >> Results/release.sha512sum
     openssl dgst -SHA3-256 Builds/$loc/u-boot-rockchip.bin && openssl dgst -SHA3-256 Builds/$loc/u-boot-rockchip.bin >> Results/release.sha3sum
     sha512sum Builds/$loc/u-boot-rockchip-spi.bin && sha512sum Builds/$loc/u-boot-rockchip-spi.bin >> Results/release.sha512sum
     openssl dgst -SHA3-256 Builds/$loc/u-boot-rockchip-spi.bin && openssl dgst -SHA3-256 Builds/$loc/u-boot-rockchip-spi.bin >> Results/release.sha3sum
   done
 done
-docker cp u-boot:/sys.info sys.info
+docker cp $NAME:/sys.info sys.info
+stop $NAME
 
-stop u-boot
 snap disable docker
 rm -f -r /var/snap/docker/*
 rm -f -r /var/snap/docker
@@ -204,7 +224,7 @@ snap remove docker --purge
 networkctl delete docker0
 rm -f -r /var/lib/snapd/cache/*
 
-scan_using_grype ubuntu.25.04 "/ --select-catalogers debian"
+scan_using_grype ubuntu.25.04 "/ --select-catalogers debian" $3
 
 snap remove syft --purge && rm -f -r $HOME/.cache/syft
 snap remove grype --purge
@@ -213,7 +233,7 @@ rm /root/getter* -f -r && rm /root/grype-scratch* -f -r && rm /root/Library -f -
 if [ "$3" = "no" ]; then
   for dev in $LIST
   do
-    for loc in $dev $dev-SB $dev-MU-SB
+    for loc in $dev $dev-SB $dev-TPM-SB $dev-MU-SB
     do
       pushd Builds/$loc/
       dd if=/dev/zero of=/dev/mmcblk1 bs=1M count=100 status=progress
@@ -231,7 +251,10 @@ if [ "$3" = "no" ]; then
     done
   done
   dd if=/dev/zero of=/dev/mmcblk1 bs=1M count=100 status=progress
-  dd if=Builds/RP64-rk3399-SB/u-boot-rockchip.bin of=/dev/mmcblk1 seek=64 conv=notrunc status=progress
+  dd if=Builds/RP64-rk3399-TPM-SB/sdcard.img of=/dev/mmcblk1 conv=notrunc status=progress
+else
+  dd if=/dev/zero of=/dev/mmcblk1 bs=1M count=100 status=progress
+  dd if=Builds/RP64-rk3399-TPM-SB/u-boot-rockchip.bin of=/dev/mmcblk1 seek=64 conv=notrunc status=progress
 fi
 pushd Results/
   sed -i 's/Builds/..\/Builds/g' release.sha512sum
