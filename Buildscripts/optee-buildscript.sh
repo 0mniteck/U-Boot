0ffa092ec6e3aae0d86d21c40d331b1a0f5a9d65
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-09-21 02:51:22 -0700

    Update optee-buildscript.sh

--- Buildscripts/optee-buildscript.sh
+++ Buildscripts/optee-buildscript.sh
@@ -48,7 +48,7 @@ do
     sed -i "3d;12d;17d;20d;22,29d;36d;83,97d;104,105d;110,309d" sub.mk
     sed -i "11iexport CC=gcc" sub.mk
     sed -i "s'-DMATH_LIB=TEE'-DMATH_LIB=TpmBigNum'" sub.mk
-    sed -i "s'cppflags-y += -DGCC -DSIMULATION=NO -DVTPM'cppflags-y += -DGCC -DVTPM=YES'" sub.mk
+    sed -i "s'-DGCC -DSIMULATION=NO -DVTPM'-DGCC -DVTPM=YES'" sub.mk
     sed -i "15icppflags-y += -DBN_MATH_LIB=Ossl -DALG_SM4=YES" sub.mk
     sed -i "24iglobal-incdirs_ext-y += \$(CFG_MS_TPM_20_REF)/TPMCmd/tpm/cryptolibs/TpmBigNum/include" sub.mk
     sed -i "24iglobal-incdirs_ext-y += \$(CFG_MS_TPM_20_REF)/TPMCmd/tpm/cryptolibs/Ossl/include" sub.mk
