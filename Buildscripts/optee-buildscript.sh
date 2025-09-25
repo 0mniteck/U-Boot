24b835f876f0d3e7aa3d5bc3ce2bde996b9a97c4
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-09-25 02:06:57 -0700

    Update optee-buildscript.sh

--- Buildscripts/optee-buildscript.sh
+++ Buildscripts/optee-buildscript.sh
@@ -29,9 +29,9 @@ do
     sed -i "s'XYZ 'OMTK'" Platform/src/VendorInfo.c
     sed -i "s'xCG 'xTCG'" Platform/src/VendorInfo.c
     sed -i "s'\\\\0\\\\0\\\\0\\\\0'TEST'" Platform/src/VendorInfo.c
-    sed -i "3i\\
-static uint32_t ByteArrayToUint32(BYTE s[4])\\
-" tpm/include/public/endian_swap.h
+#    sed -i "3i\\
+#static uint32_t ByteArrayToUint32(BYTE s[4])\\
+#" tpm/include/public/endian_swap.h
     sed -i "53i\\
         default:\\
             return StringToUint32(VENDOR_STRING_1);" Platform/src/VendorInfo.c
