e39e93427e0a24f319b5c5f91c773f92433537a4
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-09-25 06:02:33 -0700

    Update edk2-buildscript.sh

--- Buildscripts/edk2-buildscript.sh
+++ Buildscripts/edk2-buildscript.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 trap '[[ $pid ]] && kill $pid; exit' EXIT
-for plat in $ARCHS
-do
-  unzip -q $EDKP_VER.zip > /dev/null
-    source edksetup.sh
-    make -C BaseTools
-    build -n `getconf _NPROCESSORS_ONLN` -p \$ACTIVE_PLATFORM -b RELEASE -a AARCH64 -t GCC5 -D DO_X86EMU=TRUE -n `nproc`
-  popd
-done
+unzip -q $EDKP_VER.zip > /dev/null
+source edksetup.sh
+make -C BaseTools
+build -n `getconf _NPROCESSORS_ONLN` -p \$ACTIVE_PLATFORM -b RELEASE -a AARCH64 -t GCC5 -D DO_X86EMU=TRUE -n `nproc`
