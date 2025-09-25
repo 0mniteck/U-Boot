9da157badfd791b839f3119229c1508260ea8e0c
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-09-25 05:54:56 -0700

    Update edk2-buildscript.sh

--- Buildscripts/edk2-buildscript.sh
+++ Buildscripts/edk2-buildscript.sh
@@ -2,12 +2,7 @@
 trap '[[ $pid ]] && kill $pid; exit' EXIT
 for plat in $ARCHS
 do
-  unzip -q $EDKP_VER.zip -d /$plat > /dev/null
-  pushd /$plat/
-    export WORKSPACE=/root
-    export PACKAGES_PATH=/$plat/edk2-$(echo $EDK_VER):/$plat/edk2-platforms-$(echo $EDKP_VER)
-    export ACTIVE_PLATFORM='Platform/StandaloneMm/PlatformStandaloneMmPkg/PlatformStandaloneMmRpmb.dsc'
-    export GCC5_AARCH64_PREFIX=aarch64-linux-gnu-
+  unzip -q $EDKP_VER.zip > /dev/null
     source edksetup.sh
     make -C BaseTools
     build -n `getconf _NPROCESSORS_ONLN` -p \$ACTIVE_PLATFORM -b RELEASE -a AARCH64 -t GCC5 -D DO_X86EMU=TRUE -n `nproc`
