21cf2c44891dd296d7662d384aebf8128b517b48
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-09-25 05:51:32 -0700

    Update edk2-buildscript.sh

--- Buildscripts/edk2-buildscript.sh
+++ Buildscripts/edk2-buildscript.sh
@@ -2,7 +2,6 @@
 trap '[[ $pid ]] && kill $pid; exit' EXIT
 for plat in $ARCHS
 do
-  unzip -q $EDK_VER.zip -d /$plat > /dev/null
   unzip -q $EDKP_VER.zip -d /$plat > /dev/null
   pushd /$plat/
     export WORKSPACE=/root
