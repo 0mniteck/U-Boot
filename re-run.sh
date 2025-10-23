ef7e7d4f4416640d7a4085660fe943350a293e4d
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-10-23 15:08:27 -0700

    Update re-run.sh

--- re-run.sh
+++ re-run.sh
@@ -50,7 +50,7 @@ if [ "$3" = "yes" ]; then
     }
 else
   load() { # $1 Name
-    export LOAD="--load $CROSS $local_cache --target $1 --tag $1 --metadata-file Results/$1.meta.json"
+    export LOAD="--load $CROSS --target $1 --tag $1 --metadata-file Results/$1.meta.json"
     export BUILDX_METADATA_PROVENANCE=max
     export NAME=$1
     return
