6549ead49bd748f48499a83f76382ddb63a02bec
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-10-23 15:44:07 -0700

    Update re-run.sh

--- re-run.sh
+++ re-run.sh
@@ -102,7 +102,8 @@ stop() { # $1 = Name
 
 scan_using_grype() { # $1 = Name, $2 = Type:[Name], $3 = $3
   if [ "$3" != "yes" ]; then
-    pushd Results/
+    mkdir -p Results/$1
+    pushd Results/$1
       if [ -f "$HOME/.grype.yaml" ]; then GRCONF="-c $HOME/.grype.yaml"; fi
       mkdir -p "$HOME/syft" && TMPDIR="$HOME/syft" syft scan $2 -o spdx-json=$1.spdx.json
       script -q -c "grype $GRCONF sbom:$1.spdx.json -o json > $1.grype.json" $1.grype.tmp
@@ -126,6 +127,7 @@ scan_using_grype() { # $1 = Name, $2 = Type:[Name], $3 = $3
       cat $1.grype.status
     popd
   else
+    mkdir -p Results/$1
     return
   fi
 }
