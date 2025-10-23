1284b526593a488efe9335c6f97988f07f4bd1e5
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-10-23 01:40:12 -0700

    Update edk2-buildscript.sh

--- Buildscripts/edk2-buildscript.sh
+++ Buildscripts/edk2-buildscript.sh
@@ -4,5 +4,5 @@ unzip -q $EDKP_VER.zip > /dev/null
 pushd /edk2-$EDK_VER
   source edksetup.sh
   make -C BaseTools
-  build -p \$ACTIVE_PLATFORM -b RELEASE -a AARCH64 -t GCC5 -D DO_X86EMU=TRUE -n `nproc`
+  build -p \$ACTIVE_PLATFORM -b RELEASE -a AARCH64 -t GCC5 -n `nproc`
 popd
