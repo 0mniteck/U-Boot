edc4ab6a2a9e9b626abf24a62cd9e1cac98f24e9
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-10-24 15:32:45 -0700

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -19,7 +19,7 @@ if [ "$1" = "pre.cleanup" ]; then
     find . ! -type d -delete
     for con in edk2 arm-trusted optee-os u-boot ubuntu.25.04
       do
-        mkdir -p $loc
+        mkdir -p $con
         touch $con/tmp
       done
   popd
