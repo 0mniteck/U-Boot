64fe2e1474fa43303fe2473716a190b93bac9b0f
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-10-24 13:41:43 -0700

    Update edk2-buildscript.sh

--- Buildscripts/edk2-buildscript.sh
+++ Buildscripts/edk2-buildscript.sh
@@ -2,9 +2,6 @@
 trap '[[ $pid ]] && kill $pid; exit' EXIT
 unzip -q $EDKP_VER.zip > /dev/null
 pushd /edk2-$EDK_VER
-  aarch64-linux-gnu-gcc --version
-  sed -i "s'Pccts/antlr/antlr -CC -e3 -ck 3 -k 2'Pccts/antlr/antlr -CC -e3 -ck 3 -k 3'" BaseTools/Source/C/VfrCompile/GNUmakefile
-  cat BaseTools/Source/C/VfrCompile/GNUmakefile
   source edksetup.sh
   make -C BaseTools
   build -p $ACTIVE_PLATFORM -b RELEASE -a AARCH64 -t GCC5 -n `nproc`
