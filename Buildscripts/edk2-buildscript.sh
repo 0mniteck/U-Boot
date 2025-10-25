4df37cd9a3bd6343d8287df6814d59cf6f98375c
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-10-25 14:52:39 -0700

    Update edk2-buildscript.sh

--- Buildscripts/edk2-buildscript.sh
+++ Buildscripts/edk2-buildscript.sh
@@ -3,7 +3,7 @@ trap '[[ $pid ]] && kill $pid; exit' EXIT
 unzip -q $EDKP_VER.zip > /dev/null
 pushd /edk2-$EDK_VER
   sed -i "19,20d;22,24d" BaseTools/Source/C/VfrCompile/GNUmakefile
-  sed -i "176d;179,182d" BaseTools/Source/C/Makefiles/header.makefile
+  sed -i "179d;182,185d" BaseTools/Source/C/Makefiles/header.makefile
   source edksetup.sh
   make -C BaseTools
   build -p $ACTIVE_PLATFORM -b RELEASE -a AARCH64 -t GCC5 -n `nproc`
