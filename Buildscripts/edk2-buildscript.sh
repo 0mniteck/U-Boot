6139f7ba0b6b816477b5da70244c49833c2852e8
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-10-25 01:09:53 -0700

    Update edk2-buildscript.sh

--- Buildscripts/edk2-buildscript.sh
+++ Buildscripts/edk2-buildscript.sh
@@ -4,7 +4,7 @@ unzip -q $EDKP_VER.zip > /dev/null
 pushd /edk2-$EDK_VER
   # Remove if statements that are not nessasary assuming clang or gcc
   sed -i "19,20d;22,24d" BaseTools/Source/C/VfrCompile/GNUmakefile
-  sed -i "76d;79d;82d" BaseTools/Source/C/Makefiles/header.makefile
+  sed -i "176d;179,182d" BaseTools/Source/C/Makefiles/header.makefile
   source edksetup.sh
   make -C BaseTools
   build -p $ACTIVE_PLATFORM -b RELEASE -a AARCH64 -t GCC5 -n `nproc`
