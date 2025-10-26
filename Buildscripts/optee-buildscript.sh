1bd85b0d068f0a39ad750737fb105373240fe0f1
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-10-26 03:23:48 -0700

    Update optee-buildscript.sh

--- Buildscripts/optee-buildscript.sh
+++ Buildscripts/optee-buildscript.sh
@@ -96,7 +96,7 @@ BOOL  s_physicalPresence;" >> Platform/src/PlatformData.c
     sed -i "3d;12d;17d;20d;22,29d;36d;39,79d;83,97d;103,105d;110,309d" sub.mk
     sed -i "11iexport CC=gcc" sub.mk
     sed -i "s'-DMATH_LIB=TEE'-DMATH_LIB=TpmBigNum'" sub.mk
-    sed -i "s'-D_ARM_'-D__aarch64__'" sub.mk
+    sed -i "s'-D_ARM_'-D_ARM_ -D__aarch64__'" sub.mk
     sed -i "s'-DGCC -DSIMULATION=NO -DVTPM'-DGCC -DRUNTIME_SIZE_CHECKS=NO -DVTPM=YES'" sub.mk
     sed -i "15icppflags-y += -DBN_MATH_LIB=Ossl -DALG_SM4=YES" sub.mk
     sed -i "25iglobal-incdirs_ext-y += \$(CFG_MS_TPM_20_REF)/TPMCmd/tpm/cryptolibs/TpmBigNum/include" sub.mk
