67cf1ed87deda7f0d49d3d009456cc9930763553
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-10-26 03:25:27 -0700

    Update optee-buildscript.sh

--- Buildscripts/optee-buildscript.sh
+++ Buildscripts/optee-buildscript.sh
@@ -350,7 +350,7 @@ srcs_ext-y += support/TableDrivenMarshal.c
 srcs_ext-y += support/TableMarshalData.c
 srcs_ext-y += support/TpmFail.c
 srcs_ext-y += support/TpmSizeChecks.c" >> sub.mk
-    # cat sub.mk
+    cat sub.mk
     # cat include/fTPM.h
     # CFG_CORE_DYN_SHM=y CFG_SCTLR_ALIGNMENT_CHECK=n
     make -j $(nproc) VERBOSE=1 TA_DEV_KIT_DIR=/$plat/optee_os-$OPT_VER/out/arm-plat-rockchip/export-ta_arm64 CFG_MS_TPM_20_REF=/$plat/TPM CFG_TA_MEASURED_BOOT=y CFG_USER_TA_TARGETS=ta_arm64 CFG_TA_EVENT_LOG_SIZE=1024 CFG_TA_LIBGCC=y CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE32=arm-linux-gnueabihf- CROSS_COMPILE_ta_arm32=arm-linux-gnueabihf- CROSS_COMPILE_ta_arm64=aarch64-linux-gnu- O=out
