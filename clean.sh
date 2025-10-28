a84827206af78a58aae20cb9da9788a8cd92e3d7
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-10-28 03:18:15 -0700

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -29,6 +29,7 @@ if [ "$1" = "pre.cleanup" ]; then
     done
   popd
   pushd Results/
+    cp Results/release.sha512sum /tmp/release.last.sha512sum
     find . ! -type d -delete
     for con in edk2 arm-trusted optee u-boot ubuntu.25.04
       do
