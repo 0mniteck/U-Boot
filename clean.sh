449d2c6710c653c5f09dd63d0469befcc3a56994
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-10-27 21:19:14 -0700

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -5,8 +5,7 @@ if [ "$1" = "git.cleanup" ]; then
   git clean -xfd
   echo "Fetching recent changes..."
   git remote remove origin && git remote add origin git@UBoot:0mniteck/U-Boot.git
-  git pull
-  git branch --set-upstream-to=origin/$(git rev-parse --abbrev-ref HEAD) $(git rev-parse --abbrev-ref HEAD)
+  git pull && git branch --set-upstream-to=origin/$(git rev-parse --abbrev-ref HEAD) $(git rev-parse --abbrev-ref HEAD)
   mkdir -p .git/Cache
 fi
 
