3bbad04962de8cf50e340d52d7fa08b6d3ec0ba5
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-10-28 04:41:07 -0700

    Update re-run.sh

--- re-run.sh
+++ re-run.sh
@@ -104,7 +104,6 @@ stop() { # $1 = Name
 scan_using_grype() { # $1 = Name, $2 = Type:[Name], $3 = $3
   if [ "$3" != "yes" ]; then
     pushd Results/$1
-      docker buildx imagetools inspect $1 --format "{{ json .SBOM.SPDX }}" > $1.TEST.spdx.json
       docker buildx imagetools inspect $1 --format "{{ json .Provenance.SLSA }}" > $1.TEST.slsa.json
       if [ -f "$HOME/.grype.yaml" ]; then GRCONF="-c $HOME/.grype.yaml"; fi
       mkdir -p "$HOME/syft" && TMPDIR="$HOME/syft" syft scan $2 -o spdx-json=$1.spdx.json
