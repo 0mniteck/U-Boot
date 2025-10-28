6311b0e8225e3fa6091afe58f3e270446aac001b
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-10-28 15:49:06 -0700

    Update optee-buildscript.sh

--- Buildscripts/optee-buildscript.sh
+++ Buildscripts/optee-buildscript.sh
@@ -6,9 +6,9 @@ pushd /openssl-openssl-$SSL_VER/
   sed -i "1,15d" build.info
   sed -i "s'MAJOR=.'MAJOR=1'" VERSION.dat
   sed -i "s'MINOR=.'MINOR=1'" VERSION.dat
-  sed -i "s'PATCH=.'PATCH=0'" VERSION.dat
+  sed -i "s'PATCH=.'PATCH=1'" VERSION.dat
   # sed -i "s'OPENSSL_API_LEVEL > ('OPENSSL_API_LEVEL >= ('" include/openssl/macros.h
-  ./Configure --api=1.1.0 linux-aarch64 -DOPENSSL_CONFIGURED_API=10100
+  ./Configure --api=1.1.1 linux-aarch64
   make
   cp include/crypto/sm4.h include/openssl/sm4.h
 popd
