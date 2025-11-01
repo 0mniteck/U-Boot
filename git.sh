81289ba760f5cb8fe993f5425e48eb215ea7c634
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-11-01 11:55:26 -0700

    Update git.sh

--- git.sh
+++ git.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
+yubi_check() {
+  while [ "$(echo "$(lsusb | grep Yubikey)")" = "" ]; do printf "\rPlease insert yubikey...\033[K"; done
+}
+
 do_update() {
   echo "Fetching recent changes..."
   if [ "$(echo "$(cat "$HOME/.ssh/config" | grep UBoot)")" != "" ]; then
-    while [ "$(echo "$(lsusb | grep Yubikey)")" = "" ]; do printf "\rPlease insert yubikey...\033[K"; done
+    yubi_check
     git remote remove origin && git remote add origin git@UBoot:0mniteck/U-Boot.git
     echo "" && read -p "Origin set to SSH; Continue git pull..."
   fi
   git pull $(git remote -v | awk '{ print $2 }' | tail -n 1) $(git rev-parse --abbrev-ref HEAD)
 }
+
+if [ "$1" = "check" ]; then
+yubi_check
+return 0
+fi
 
 if [ "$1" = "update" ]; then
 do_update
@@ -21,7 +30,7 @@ if [ "$(echo "$(cat "$HOME/.ssh/config" | grep UBoot)")" != "" ]; then
   ssh-add $HOME/.ssh/id_ecdsa_s*[!.pub]
 fi
 git status && git add -A && git status
-if [ "$(echo "$(cat "$HOME/.ssh/config" | grep UBoot)")" != "" ]; then while [ "$(echo "$(lsusb | grep Yubikey)")" = "" ]; do printf "\rPlease insert yubikey...\033[K"; done; fi
+if [ "$(echo "$(cat "$HOME/.ssh/config" | grep UBoot)")" != "" ]; then yubi_check; fi
 git commit -a -S -m "$1" && sleep 5 && git push --set-upstream origin $(git rev-parse --abbrev-ref HEAD):Docker
 if [ "$2" != "" ]; then
   git tag -a "$2" -s -m "Tagged Release $2" && sleep 5 && git push origin "refs/tags/$2"
