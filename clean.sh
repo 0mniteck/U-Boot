a07cd60c8be66aa743382fb38e295e38926e3543
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-11-01 09:24:14 -0700

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -22,6 +22,7 @@ if [ "$1" = "pre.cleanup" ]; then
       do
         touch $dev$loc/tmp
       done
+        touch $dev/tmp
     done
     for arch in $ARCHS
     do
@@ -77,6 +78,7 @@ if [ "$1" = "cleanup" ]; then
       do
         rm -f $dev$loc/tmp
       done
+      rm -f $dev/tmp
     done
     for arch in $ARCHS
     do
