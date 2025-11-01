bc9ea23688ebb95cb4d43c11be3b431abea26e5e
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-11-01 16:38:11 -0700

    Update git.sh

--- git.sh
+++ git.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Host UBoot ## Add to $HOME/.ssh/config for SSH support use ecdsa-sk.
+# Host UBoot ## Add to $HOME/.ssh/config for SSH support, recommend ecdsa-sk or ed25519_sk.
 #    Hostname github.com
 #    IdentityFile /root/.ssh/id_ecdsa_sk
 #    IdentitiesOnly yes
