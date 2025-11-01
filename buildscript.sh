3de0e6659bf2e6226836518a3da62b1df988d384
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-11-01 08:19:55 -0700

    Update buildscript.sh

--- buildscript.sh
+++ buildscript.sh
@@ -110,6 +110,10 @@ do
     printf "\"" >> vars.env
     echo "" >> vars.env
     printf "setenv $lis1 \"" >> vars.env
+  elif [ $lis1 = VARIANTS ]; then
+    printf "\"" >> vars.env
+    echo "" >> vars.env
+    printf "setenv $lis1 \"$'\0' " >> vars.env
   fi
   printf -- "$lis2 " >> vars.env
 done
