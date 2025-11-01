af9491587b316992e2c278f63a599459ffaed9ad
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-11-01 07:32:23 -0700

    Update buildscript.sh

--- buildscript.sh
+++ buildscript.sh
@@ -107,7 +107,7 @@ do
   lis1=$(echo $lis | cut -d'^' -f1)
   lis2=$(echo $lis | cut -d'^' -f2)
   if [ $lis1 = BUILD_LIST ] || [ $lis1 = LIST ] || [ $lis1 = ARCHS ] || [ $lis1 = VARIANTS ] || [ $lis1 = TARGETS ]; then
-    printf "\"" >> vars.env
+    printf " \"" >> vars.env
     echo "" >> vars.env
     printf "setenv $lis1 \"" >> vars.env
   fi
