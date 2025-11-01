e3f8b25d03b29009110cb25f2fd73041e8281da2
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-11-01 03:23:17 -0700

    Update buildscript.sh

--- buildscript.sh
+++ buildscript.sh
@@ -13,6 +13,8 @@ export OPT_SUM2="2bdd0820535ac57765a47b5c77b12fb83716d45d904cd8363f2463c83c067fd
 export TPM_SUM="411a186326447c4ef0dd23341f0ef6f0198efd6a8c930ad379338afef6093c43c848770a551b2f44c06e7464ac7726f3207a0799905a07ad13ce9874236ccc5d"
 export SSL_VER="3.6.0"
 export SSL_SUM="68ccaf430b932a67a8ef344d78a0f237c4a0dc9a9a75199b05cd94d40574285b134f87ac6726a8b469ba49c05f5819281f4c140cd8240ae6f3ffe686b60af3c6"
+export CROSS_VER="1.28.0"
+export CROSS_SUM="367b6115ab4b0b8222dff932838f1aeffee4127f3bcf56bbd8b960bf0d4de5ddf8e55a7f77e6e4aa2651adb892a7ffaaa4dc135aa0a8b009366de820b87486e2"
 export ROT_SUM="af17962e977e689d4f17944e36229189ea1c685ed13bfe462909c3d3f31d0c7a9ddb54c7840ea6cb33dc847cddf982aac6b53a8436b889db1834cab63bb935b2"
 export ATF_VER="lts-v2.12.7"
 export ATF_SUM="335e119c6dbca8c3e30cb08c0b45dd95022fa4f9936334ffed14b2958abd31c21791167a4145c7629525dff61bafc66b93808b68ab7d7779594270b94a1e173f"
@@ -25,7 +27,10 @@ export BUILD_LIST="R5B-rk3588:rock5b-rk3588_defconfig RP64-rk3399:rockpro64-rk33
 export LIST="R5B-rk3588 RP64-rk3399 PBP-rk3399"
 export ARCHS="rk3588 rk3399"
 
-while getopts ":a:c:d:e:m:t:w:" opt; do
+export VARIANTS="\$dev \$dev-SB \$dev-TPM-SB \$dev-MU-SB"
+export TARGETS="edk2 arm-trusted optee u-boot ubuntu"
+
+while getopts ":a:c:d:e:m:t:w:z:" opt; do
   case $opt in
   a) # Alternate List (yes/No)
     ALT="$OPTARG"
@@ -47,6 +52,9 @@ while getopts ":a:c:d:e:m:t:w:" opt; do
     ;;
   w) # Cross Compile (yes/No)
     CROSS="$OPTARG"
+    ;;
+  z) # Target Selection ("target1,target2,all")
+    TARGET="$OPTARG"
     ;;
   \?)
     echo "Invalid option: -$opt" >&2
@@ -77,12 +85,15 @@ if [ "$ALT" = "yes" ]; then
   export LIST="PT2-rk3566"
   export ARCHS="rk3568"
 fi
+if [ "$TARGET" = "" || "$TARGET" = "all" ]; then
+  TARGET="$TARGETS"
+fi
 if [ "$CLEAN" = "yes" ]; then
   ./clean.sh git.cleanup
 fi
 
 > vars.env
-for env in HUB^$HUB BASE^$BASE BASE_EXTRA^$BASE_EXTRA EDK_VER^$EDK_VER EDKP_VER^$EDKP_VER EDKP_SUM^$EDKP_SUM OPT_VER^$OPT_VER OPT_SUM^$OPT_SUM OPT_SUM2^$OPT_SUM2 TPM_SUM^$TPM_SUM SSL_VER^$SSL_VER SSL_SUM^$SSL_SUM ROT_SUM^$ROT_SUM ATF_VER^$ATF_VER ATF_SUM^$ATF_SUM MTLS_VER^$MTLS_VER MTLS_SUM^$MTLS_SUM UB_VER^$UB_VER UB_SUM^$UB_SUM
+for env in HUB^$HUB BASE^$BASE BASE_EXTRA^$BASE_EXTRA EDK_VER^$EDK_VER EDKP_VER^$EDKP_VER EDKP_SUM^$EDKP_SUM OPT_VER^$OPT_VER OPT_SUM^$OPT_SUM OPT_SUM2^$OPT_SUM2 TPM_SUM^$TPM_SUM SSL_VER^$SSL_VER SSL_SUM^$SSL_SUM CROSS_VER^$CROSS_VER CROSS_SUM^$CROSS_SUM ROT_SUM^$ROT_SUM ATF_VER^$ATF_VER ATF_SUM^$ATF_SUM MTLS_VER^$MTLS_VER MTLS_SUM^$MTLS_SUM UB_VER^$UB_VER UB_SUM^$UB_SUM
 do
   env1=$(echo $env | cut -d'^' -f1)
   env2=$(echo $env | cut -d'^' -f2)
@@ -91,11 +102,11 @@ do
 done
 
 printf "\"" >> vars.env
-for lis in BUILD_LIST^$BUILD_LIST LIST^$LIST ARCHS^$ARCHS
+for lis in BUILD_LIST^$BUILD_LIST LIST^$LIST ARCHS^$ARCHS VARIANTS^$VARIANTS TARGETS^$TARGETS
 do
   lis1=$(echo $lis | cut -d'^' -f1)
   lis2=$(echo $lis | cut -d'^' -f2)
-  if [ $lis1 = BUILD_LIST ] || [ $lis1 = LIST ] || [ $lis1 = ARCHS ]; then
+  if [ $lis1 = BUILD_LIST ] || [ $lis1 = LIST ] || [ $lis1 = ARCHS ] || [ $lis1 = VARIANTS ] || [ $lis1 = TARGETS ]; then
     printf "\"" >> vars.env
     echo "" >> vars.env
     printf "setenv $lis1 \"" >> vars.env
@@ -109,6 +120,7 @@ echo "Clean Build: $CLEAN"
 echo "Tag Release: $TAG"
 echo "Developer Build: $DEV"
 echo "Using Alternate List: $ALT"
+echo "Targeting: $TARGET"
 if [ "$EPOCH" = "" ]; then
   echo "Override Source Epoch: $(cat Results/release.sha512sum | grep Epoch | cut -d ' ' -f5)"
 else
@@ -130,7 +142,7 @@ if [ "$CLEAN" = "yes" ]; then
 fi
 
 sudo apt update && sudo apt install -y bc dosfstools parted screen snapd systemd-cryptsetup
-> builder.log && sudo screen -c vars.env -L -Logfile builder.log bash -c './re-run.sh '$EPOCH' '$CLEAN' '$DEV' '$CROSS' '$MOUNT
+> builder.log && sudo screen -c vars.env -L -Logfile builder.log bash -c './re-run.sh '$EPOCH' '$CLEAN' '$DEV' '$CROSS' '$MOUNT' '$TARGET
 echo "" && cat builder.log | grep -n "Checksum Matched! " && echo "" && cat Results/release.sha512sum && echo "" && cat Results/release.sha3sum && echo ""
 mv builder.log Results/builder.log && status="$(cat status.build)" && ./clean.sh cleanup && ls -la Builds/*
 read -p "$status: --> sign/commit/push"
