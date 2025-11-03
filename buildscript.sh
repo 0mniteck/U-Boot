88bb5e23bf5a9da5228a8d601de826e98eea0467
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-11-03 00:34:15 -0800

    Update buildscript.sh

--- buildscript.sh
+++ buildscript.sh
@@ -154,7 +154,7 @@ if [ "$CLEAN" = "yes" ]; then
   ./clean.sh dir.cleanup
 fi
 
-> builder.log && sudo screen -c vars.env -L -Logfile builder.log bash -c './re-run.sh '$EPOCH' '$CLEAN' '$DEV' '$CROSS' '$MOUNT' '$TARGET' '
+> builder.log && sudo screen -c vars.env -L -Logfile builder.log bash -c "./re-run.sh "$EPOCH" "$CLEAN" "$DEV" "$CROSS" "$MOUNT" "$TARGET" "
 
 echo "" && cat builder.log | grep -n "Checksum Matched! " && echo ""
 cat Results/release.sha512sum && echo "" && cat Results/release.sha3sum && echo ""
