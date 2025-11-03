cb36b5701d58faf830eb37eca3c7a510ed85900d
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-11-02 23:16:46 -0800

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -60,38 +60,38 @@ fi
 
 if [[ "$1" == *cleanup.docker* ]]; then
   if [[ "$1" == *cleanup.docker.remove* ]]; then
-    snap disable docker
+    snap disable docker 2>/dev/null && wait
     rm -f -r /var/snap/docker/*
     rm -f -r /var/lib/snapd/cache/*
     sleep 5
   fi
-  if [[ "$1" == *cleanup.docker.unmount* ]]; then
-    snap disable docker
-    umount -f /dev/mapper/Luks-Signal
+  if [[ "$2" == *unmount* ]]; then
+    snap disable docker 2>/dev/null && wait
+    umount -f /dev/mapper/Luks-Signal 2>/dev/null && wait
     sleep 5
-    systemd-cryptsetup detach Luks-Signal
+    systemd-cryptsetup detach Luks-Signal 2>/dev/null && wait
   fi
   if [[ "$1" == *cleanup.docker.remove* ]]; then
     rm -f -r /var/snap/docker
-    snap enable docker
-    snap remove docker --purge
-    snap remove docker --purge
-    snap remove core24 --purge
+    snap enable docker 2>/dev/null && wait
+    snap remove docker --purge 2>/dev/null && wait
+    snap remove docker --purge 2>/dev/null && wait
+    snap remove core24 --purge 2>/dev/null && wait
   else
-    snap enable docker
-    snap remove docker
+    snap enable docker 2>/dev/null && wait
+    snap remove docker 2>/dev/null && wait
   fi
   if [[ $(snap list) == *disabled* ]]; then
     snap list
   fi
-  networkctl delete docker0
-  networkctl delete docker1
+  networkctl delete docker0 2>/dev/null && wait
+  networkctl delete docker1 2>/dev/null && wait
 fi
 
 if [[ "$1" == *cleanup.snaps* ]]; then
   if [[ "$1" == *cleanup.snaps.remove* ]]; then
-    snap remove syft --purge
-    snap remove grype --purge
+    snap remove syft --purge 2>/dev/null && wait
+    snap remove grype --purge 2>/dev/null && wait
   fi
   rm /root/getter* -f -r && rm /root/grype-scratch* -f -r && rm /root/syft -f -r && rm /root/6 -f -r && rm /root/Library -f -r && rm -f -r $HOME/.cache/grype && rm -f -r $HOME/.cache/syft && rm -f -r /tmp/grype-scratch* && rm -f -r /tmp/getter*
 fi
