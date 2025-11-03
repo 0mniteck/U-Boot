9cae25c9b4760aa7cb3328b17e87a4c6d88a520d
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-11-02 17:36:26 -0800

    Update buildscript.sh

--- buildscript.sh
+++ buildscript.sh
@@ -154,7 +154,7 @@ if [ "$CLEAN" = "yes" ]; then
   ./clean.sh dir.cleanup
 fi
 
-sudo apt update && sudo apt install -y bc dosfstools parted screen snapd systemd-cryptsetup
+sudo apt update && sudo apt upgrade -y && sudo apt install -y bc dosfstools parted screen snapd systemd-cryptsetup
 > builder.log && sudo screen -c vars.env -L -Logfile builder.log bash -c './re-run.sh '$EPOCH' '$CLEAN' '$DEV' '$CROSS' '$MOUNT' '$TARGET' '
 echo "" && cat builder.log | grep -n "Checksum Matched! " && echo "" && cat Results/release.sha512sum && echo "" && cat Results/release.sha3sum && echo ""
 cat build.info >> Results/build.info && cat Results/build.info && echo ""
