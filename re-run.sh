97e37ba55787ac17521ceb1b3afa32703ccb40b3
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-11-03 20:26:26 -0800

    Update re-run.sh

--- re-run.sh
+++ re-run.sh
@@ -5,7 +5,7 @@ if [ "$TARGETS" != "" ]; then
   echo "TARGET: $TARGETS"
   export TARGET="$TARGETS"
 fi
-if [ "$6" != "no" ]; then
+if [ "$6" = "yes" ]; then
   echo "CHECK REPRODUCIBILITY: $6"
   export check_file=1
 fi
