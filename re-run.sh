d3113a1b3266f8d54e9c8b81d2ef66dba7e24e3d
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-11-03 22:02:03 -0800

    Update re-run.sh

--- re-run.sh
+++ re-run.sh
@@ -306,23 +306,23 @@ load u-boot
 if [[ "$TARGET" == *$NAME* ]]; then
   if [ "$3" = "no" ]; then
     mkfs.fat -i 00000000 -n "U-BOOT" --invariant -C /tmp/sdcard.img 35000
-    echo "dc3272192cb9339d79bc1b8d6066ba056e2d7f952313688085f8009d221f692842f6cd0f378bd83c22dfcd2b5b266e8b0c6805ae5c9a05b4ffe0edefe7b4d079  /tmp/sdcard.img" | sha512sum -c -- && wait || exit 1
     for dev in $LIST
     do
       for loc in $VARIANTS
       do
         pushd Builds/$dev$loc/
-        cp /tmp/sdcard.img sdcard.img && mount sdcard.img /mnt
-        cp u-boot-rockchip.bin /mnt/u-boot-rockchip.bin
-        cp u-boot-rockchip-spi.bin /mnt/u-boot-rockchip-spi.bin
-        touch -d "$(date -R -d $source_date)" /mnt/*
-        touch -d "$(date -R -d $source_date)" /mnt
-        touch -c -d "$(date -R -d $source_date)" /mnt/*
-        touch -c -d "$(date -R -d $source_date)" /mnt
-        touch -a -d "$(date -R -d $source_date)" /mnt/*
-        touch -a -d "$(date -R -d $source_date)" /mnt
-        dd if=/mnt/u-boot-rockchip.bin of=sdcard.img seek=64 conv=notrunc status=progress
-        sync && umount /mnt
+          echo "dc3272192cb9339d79bc1b8d6066ba056e2d7f952313688085f8009d221f692842f6cd0f378bd83c22dfcd2b5b266e8b0c6805ae5c9a05b4ffe0edefe7b4d079  /tmp/sdcard.img" | sha512sum -c -- && wait || exit 1
+          cp /tmp/sdcard.img sdcard.img && mount sdcard.img /mnt
+          cp u-boot-rockchip.bin /mnt/u-boot-rockchip.bin
+          cp u-boot-rockchip-spi.bin /mnt/u-boot-rockchip-spi.bin
+          touch -d "$(date -R -d $source_date)" /mnt/*
+          touch -d "$(date -R -d $source_date)" /mnt
+          touch -c -d "$(date -R -d $source_date)" /mnt/*
+          touch -c -d "$(date -R -d $source_date)" /mnt
+          touch -a -d "$(date -R -d $source_date)" /mnt/*
+          touch -a -d "$(date -R -d $source_date)" /mnt
+          dd if=/mnt/u-boot-rockchip.bin of=sdcard.img seek=64 conv=notrunc status=progress
+          sync && umount /mnt
         popd
         sha512sum Builds/$dev$loc/sdcard.img >> Results/release.sha512sum
       done
