9134b30910b9da3fc554b228cc75708a01d1b479
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-11-06 02:45:01 -0800

    Update re-run.sh

--- re-run.sh
+++ re-run.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/pkexec /bin/bash
+echo $PWD
 mv build.info tmp && echo "Starting Build: $(date -u '+on %D at %R UTC')" > build.info && cat tmp >> build.info && rm -f tmp
 echo "Starting Build: $(date -u '+on %D at %R UTC')"
 pushd ..
