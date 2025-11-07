903d1e31905499131e5f175dee5f897c5258789e
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-11-07 03:56:20 -0800

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -1,35 +1,16 @@
-#!/usr/bin/pkexec /bin/bash
+#!/bin/bash
 
 ## Available Commands:
 # git.cleanup
 # git.cleanup.(cache)
-# dir.cleanup
-# cleanup.docker (unmount)
-# cleanup.docker.(remove) (unmount)
-# cleanup.snaps
-# cleanup.snaps.(remove)
 # tmp.cleanup
 
-cd $3
-source defaults
-
-do_update() {
-  ./git.sh update
-}
 do_clean() {
   ./git.sh clean
 }
-
-apt_update() {
-  apt update
-  apt upgrade -y
-  apt install -y bc dosfstools parted screen snapd systemd-cryptsetup
+do_update() {
+  ./git.sh update
 }
-
-if [[ "$1" == *apt.update* ]]; then
-  apt_update
-fi
-
 if [[ "$1" == *git.cleanup* ]]; then
   do_clean
   do_update
@@ -67,46 +48,6 @@ if [[ "$1" == *git.cleanup* ]]; then
       done
   popd
 fi
-
-if [[ "$1" == *cleanup.docker* ]]; then
-  if [[ "$1" == *cleanup.docker.remove* ]]; then
-    snap disable docker 2>/dev/null && wait
-    rm -f -r /var/snap/docker/*
-    rm -f -r /var/lib/snapd/cache/*
-    sleep 5
-  fi
-  if [[ "$2" == *unmount* ]]; then
-    snap disable docker 2>/dev/null && wait
-    umount -f /dev/mapper/Luks-Signal 2>/dev/null && wait
-    sleep 5
-    systemd-cryptsetup detach Luks-Signal 2>/dev/null && wait
-  fi
-  if [[ "$1" == *cleanup.docker.remove* ]]; then
-    rm -f -r /var/snap/docker
-    snap enable docker 2>/dev/null && wait
-    snap remove docker --purge 2>/dev/null && wait
-    snap remove docker --purge 2>/dev/null && wait
-    snap remove core24 --purge 2>/dev/null && wait
-  else
-    snap enable docker 2>/dev/null && wait
-    snap remove docker 2>/dev/null && wait
-  fi
-  if [[ $(snap list) == *disabled* ]]; then
-    snap list
-  fi
-  networkctl delete docker0 2>/dev/null && wait
-  networkctl delete docker1 2>/dev/null && wait
-fi
-
-if [[ "$1" == *cleanup.snaps* ]]; then
-  if [[ "$1" == *cleanup.snaps.remove* ]]; then
-    snap remove syft --purge 2>/dev/null && wait
-    snap remove grype --purge 2>/dev/null && wait
-    rm -f -r /root/Library
-  fi
-  rm -f -r /root/getter* && rm -f -r /root/grype-scratch* && rm -f -r /root/syft && rm -f -r /root/6 && rm -f -r $HOME/.cache/grype && rm -f -r $HOME/.cache/syft && rm -f -r /tmp/grype-scratch* && rm -f -r /tmp/getter* && rm -f $HOME/.grype.yaml
-fi
-
 if [ "$1" = "tmp.cleanup" ]; then
   pushd Builds/
     for dev in $LIST
