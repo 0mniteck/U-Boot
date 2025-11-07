a469c6bfe5370a0220005eec5ee5c9f92cbe43f1
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-11-06 22:27:42 -0800

    Update re-run.sh

--- re-run.sh
+++ re-run.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/pkexec /bin/bash
+
 cd $7
-echo $PWD
+
 mv build.info tmp && echo "Starting Build: $(date -u '+on %D at %R UTC')" > build.info && cat tmp >> build.info && rm -f tmp
 echo "Starting Build: $(date -u '+on %D at %R UTC')"
+
 pushd ..
   ARCHS=$(echo $ARCHS | tr ' ' '\n' | sort -u | tr '\n' ' ')
   TARGETS=$(echo $TARGETS | tr ' ' '\n' | sort -u | tr '\n' ' ')
