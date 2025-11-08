10d5b42f4ab924f7513014ad98a337513c75b631
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-11-08 07:58:39 -0800

    Update re-run.sh

--- re-run.sh
+++ re-run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source ../defaults 2>/dev/null
-source ../defaults.set 2>/dev/null
+source ../*.set 2>/dev/null && rm -f ../*.set
 env
 mv build.info tmp && echo "Starting Build: $(date -u '+on %D at %R UTC')" > build.info && cat tmp >> build.info && rm -f tmp
 echo "Starting Build: $(date -u '+on %D at %R UTC')"
