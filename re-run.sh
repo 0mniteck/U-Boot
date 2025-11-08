20a9bac11942ed9df6cb3a532616b7b502a6e52e
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-11-08 05:45:24 -0800

    Update re-run.sh

--- re-run.sh
+++ re-run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source ../defaults
+env
 mv build.info tmp && echo "Starting Build: $(date -u '+on %D at %R UTC')" > build.info && cat tmp >> build.info && rm -f tmp
 echo "Starting Build: $(date -u '+on %D at %R UTC')"
 ARCHS=$(echo $ARCHS | tr ' ' '\n' | sort -u | tr '\n' ' ')
