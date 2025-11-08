629a546747ec611581744aa03250cb05da47dae7
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-11-08 10:18:27 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ add_user() { #1 = $(whoami)
   chmod 660 /var/run/docker.sock
   mkdir -p /var/snap/docker/common/var-lib-docker/tmp/
   mkdir -p /var/snap/docker/common/var-lib-docker/overlay2/l/
-  mkdir -p /var/snap/docker/common/var-lib-docker/image/overlay2/imagedb/content/
+  mkdir -p /var/snap/docker/common/var-lib-docker/image/overlay2/imagedb/content/sha256/
 }
 
 do_check() {
