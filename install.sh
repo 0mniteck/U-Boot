b045182d1522e553602ae100ec68e5e3776eddc2
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-11-10 12:29:52 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -59,7 +59,7 @@ Press any key to continue. Press CTRL+C to exit..."
 
 crypt_mount() { #1 = device
   do_check
-  printf '\t' | systemd-cryptsetup attach Luks-Signal /dev/$1 && wait && sleep 1
+  systemd-cryptsetup attach Luks-Signal /dev/$1 && wait && sleep 1
   mount /dev/mapper/Luks-Signal /var/snap/docker && wait
 }
 
