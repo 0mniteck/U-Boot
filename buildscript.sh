43a97f0ab3cc4680c3721f2dc326e93958a0218f
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-11-09 22:33:53 -0800

    Update buildscript.sh

--- buildscript.sh
+++ buildscript.sh
@@ -77,7 +77,7 @@ fi
 if [[ $(which pkexec) = "" ]]; then
   sudo apt update && sudo apt upgrade -y && sudo apt install -y bc dosfstools parted pkexec screen snapd systemd-cryptsetup
   sudo -K
-elif [[ $(which bc) != "" && $(which dosfstools) != "" && $(which parted) != "" && $(which screen) != "" && $(which snapd) != "" && $(which systemd-cryptsetup) != "" && "$CLEAN" = "no"]]; then
+elif [[ $(which bc) != "" && $(which dosfstools) != "" && $(which parted) != "" && $(which screen) != "" && $(which snapd) != "" && $(which systemd-cryptsetup) != "" && "$CLEAN" = "no" ]]; then
   wait
 else
   $PWD/install.sh apt.update
