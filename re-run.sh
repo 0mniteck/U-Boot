c4326f1b9b24de10d949c47608596e144def5375
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-11-09 16:32:55 -0800

    Update re-run.sh

--- re-run.sh
+++ re-run.sh
@@ -66,7 +66,7 @@ stop() { # $1 = Name
 scan_using_grype() { # $1 = Name, $2 = Type:[Name]
   if [ "$DEV" != "yes" ]; then
     pushd Results/$1
-      mkdir -p "~/.cache/syft/tmp" && TMPDIR="~/.cache/syft/tmp" SYFT_CACHE_DIR="~/.cache/syft" syft scan $2 -o spdx-json=$1.spdx.json
+      TMPDIR="/var/snap/docker/tmp" SYFT_CACHE_DIR="/var/snap/docker/tmp" syft scan $2 -o spdx-json=$1.spdx.json
       script -q -c "grype $GRCONF sbom:$1.spdx.json -o json > $1.grype.json" $1.grype.tmp.tmp > $1.grype.tmp
       marker() { # $1 = Name, $2 = Order, $3 = Marker/ID
         grep "$3" $1.grype.tmp | tail -n 1 > $1.grype.status.$2
