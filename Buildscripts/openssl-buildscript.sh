e9cfb0c3b578954a61cfe49762bc85cbde26a587
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-11-13 14:24:26 -0800

    Create openssl-buildscript.sh

---
+++ Buildscripts/openssl-buildscript.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+unzip -q SSL.zip -d / > /dev/null
+mv /openssl-openssl-$SSL_VER /SSL
+rm -f -r /usr/include/openssl
+pushd /SSL
+  sed -i "1,15d" build.info
+  sed -i "s'MAJOR=.'MAJOR=1'" VERSION.dat
+  sed -i "s'MINOR=.'MINOR=1'" VERSION.dat
+  sed -i "s'PATCH=.'PATCH=0'" VERSION.dat
+  ./Configure --api=1.1.0 linux-aarch64
+  make
+  cp include/crypto/sm4.h include/openssl/sm4.h
+popd
+mv /SSL/include /usr/include/openssl
