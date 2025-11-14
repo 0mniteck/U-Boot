aa96e05cf9df973926fc852104f8526b5b972ffe
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-11-14 02:00:24 -0800

    Update u-boot-buildscript.sh

--- Buildscripts/u-boot-buildscript.sh
+++ Buildscripts/u-boot-buildscript.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-env | sort
+env | sort >> /.env
+echo "" >> $_
 for dev in $BUILD_LIST
   do
   for loc in $(echo $dev | cut -d':' -f1): $(echo $dev | cut -d':' -f1)-SB:sb- $(echo $dev | cut -d':' -f1)-TPM-SB:tpm-sb- $(echo $dev | cut -d':' -f1)-MU-SB:mutable-sb-
