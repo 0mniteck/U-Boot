3727d165854ad7a5c0b8113df1dac0a65bf7a8cd
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-11-14 18:36:38 -0800

    Update edk2-buildscript.sh

--- Buildscripts/edk2-buildscript.sh
+++ Buildscripts/edk2-buildscript.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-env | sort >> /.env
-echo "" >> /.env
+env | sort >> /.env && echo "" >> /.env
 unzip -q $EDKP_VER.zip > /dev/null
 pushd /edk2-$EDK_VER
   sed -i "19,20d;22,24d" BaseTools/Source/C/VfrCompile/GNUmakefile
