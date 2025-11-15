c5ebdaf2ab042f071bff3bb1a849811db34fbc2d
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-11-15 06:08:14 -0800

    Update re-run.sh

--- re-run.sh
+++ re-run.sh
@@ -6,7 +6,7 @@ mv build.info tmp && echo "Starting Build: $(date -u '+on %D at %R UTC')" > buil
 echo "Starting Build: $(date -u '+on %D at %R UTC')"
 ARCHS=$(echo $ARCHS | tr ' ' '\n' | sort -u | tr '\n' ' ')
 TARGETS=$(echo $TARGETS | tr ' ' '\n' | sort -u | tr '\n' ' ')
-BUILDK="--buildkitd-config $PWD/../Includes/buildkitd.toml --driver-opt \"network=host,image=moby/buildkit:v0.25.1-rootless\" --name U-Boot-Builder"
+BUILDK="--buildkitd-config $PWD/../Includes/buildkitd.toml --driver=docker-container --driver-opt=\"network=host,image=moby/buildkit:v0.25.1-rootless\" --name U-Boot-Builder"
 set -x
 if [ "$TARGETS" != "" ]; then
   echo "TARGET: $TARGETS"
