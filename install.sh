e7fc3428bb775b3d537869f66fc9ba3fc4abcdbd
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-12-10 11:50:10 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -81,6 +81,7 @@ install.docker() { #1 = cross, #2 = device, #3 = whoami
     snap install docker --revision=3380
     # systemctl stop snap.docker.nvidia-container-toolkit
     # systemctl disable snap.docker.nvidia-container-toolkit
+    sleep 5
     snap stop docker
     sed -i "s':/root:':/home/root:'" /etc/passwd
     mkdir -p /home/root
