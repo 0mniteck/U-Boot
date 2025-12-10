ed4e4dbcbbabdbbef9f395e7349e1f9e5f4cf3ad
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-12-09 23:09:45 -0800

    Update re-run.sh

--- re-run.sh
+++ re-run.sh
@@ -2,10 +2,6 @@
 source $PWD/defaults.set 2>/dev/null && rm -f defaults.set
 source $PWD/choices.set 2>/dev/null && rm -f choices.set
 env | sort >> env/re-run.env && echo "" >> env/re-run.env
-export DOCKER_HOST=unix:///run/user/1000/docker.sock
-alias docker=/snap/docker/current/bin/docker
-docker info | grep rootless >> $HOME/log
-cat $HOME/log
 
 mv build.info tmp && echo "Starting Build: $(date -u '+on %D at %R UTC')" > build.info && cat tmp >> build.info && rm -f tmp
 echo "Starting Build: $(date -u '+on %D at %R UTC')"
@@ -84,6 +80,10 @@ else
 fi
 
 init_runner() {
+  export DOCKER_HOST=unix:///run/user/1000/docker.sock
+  alias docker=/snap/docker/current/bin/docker
+  docker info | grep rootless >> $HOME/log
+  cat $HOME/log
   if [[ "$cross" == "cross" || "$DEV" == *yes* ]]; then
     docker buildx create $CROSS $BUILDT --name U-Boot-Builder --node u-boot-builder-0 --bootstrap --use
     if [[ "$cross" = "cross" ]]; then
