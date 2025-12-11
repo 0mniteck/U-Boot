85e877c2b10f219a233da1d25937edce20564a4b
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-12-10 22:12:42 -0800

    Update re-run.sh

--- re-run.sh
+++ re-run.sh
@@ -81,7 +81,7 @@ fi
 
 init_runner() {
   export DOCKER_HOST=unix:///run/user/1000/docker.sock
-  sleep 5
+  sleep 10
   cp $HOME/tmp/log Results/logs/rootless.log
   /snap/docker/current/bin/docker info | grep rootless >> Results/logs/rootless.log
   if [[ "$cross" == "cross" || "$DEV" == *yes* ]]; then
